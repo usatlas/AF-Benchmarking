34abf51ca715bd74da7376677bffae2f6b1f2139
Juan <selbor@Juans-MacBook-Pro.local>
2024-08-20 11:01:38 -0700

    Changed ordering in file

--- EVNT/run_EVNT.sh
+++ EVNT/run_EVNT.sh
@@ -4,14 +4,6 @@ configdir=$PWD/100xxx/100001
 cp /AF-Benchmarking/EVNT/mc* $configdir
 cp /AF-Benchmarking/EVNT/SUSY_*.py $configdir
 
-export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
-source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
-asetup AthGeneration,23.6.34,here
-
-seed=1001
-
-Gen_tf.py --ecmEnergy=13000.0 --jobConfig=$configdir  --outputEVNTFile=EVNT.root --maxEvents=10000 --randomSeed=${seed}
-
 curr_time=$(date +"%Y.%m.%dT%H")
 
 if [ $HOME = "/home/$USER" ]
@@ -25,9 +17,18 @@ then
   output_dir="/usatlas/workarea/$USER/benchmarks/$curr_time/EVNT"
 fi
 
-mkdir -p ${output_dir}
+
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
+asetup AthGeneration,23.6.34,here
+
+seed=1001
+
+Gen_tf.py --ecmEnergy=13000.0 --jobConfig=$configdir  --outputEVNTFile=EVNT.root --maxEvents=10000 --randomSeed=${seed}
+
+mkdir -p $output_dir
 
 
 mv log.* ${outputdir}
 mv *.generate ${outputdir}
-mv /data/selbor/EVNT/* ${outputdir}
+mv evnt.* ${outputdir}
