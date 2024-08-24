8bfc32e396e701b65c9bea7aca4f4471452993aa
Juan <selbor@Juans-MacBook-Pro.local>
2024-08-24 14:22:49 -0700

    Added arg-parse

--- TRUTH3/run_truth3.sh
+++ TRUTH3/run_truth3.sh
@@ -5,12 +5,15 @@ if [ $HOME = "/home/$USER"]
 then
   export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
   inputdir="/data/selbor/TRUTH3_StaticDir/"
-  outputdir="/data/selbor/benchmarks/$curr_date/TRUTH3"
+  outputdir="/data/selbor/benchmarks/$curr_date/TRUTH3/"
 elif [ $HOME = "/sdf/home/s/$USER"]
 then
-  inputdir="/sdf/home/s/selbor/AF-Benchmarking/TRUTH3"
-  outputdir="/sdf/data/atlas/u/$USER/benchmarks/$curr_date/TRUTH3"
+  inputdir="/sdf/home/s/selbor/AF-Benchmarking/TRUTH3/"
+  outputdir="/sdf/data/atlas/u/$USER/benchmarks/$curr_date/TRUTH3/"
+fi
 
+# This same block works at SLAC
+# But it doesn't seem to run at BNL
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
 asetup Athena,24.0.53,here
 
