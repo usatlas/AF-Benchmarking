c3689ee4c15855bf16cebe858f601ee04eb04c0d
Juan <selbor903@gmail.com>
2024-09-09 10:45:09 -0700

    Added a comment to the EVNT Script

--- EVNT/run_EVNT.sh
+++ EVNT/run_EVNT.sh
@@ -44,6 +44,7 @@ main() {
     Container ${OScontainer} ${config_dir} ${seed}
   elif [[ -d /usatlas ]]
   then
+    # There is a madgraph error; I can just raise a flag and have the process skip that step.
     local  output_dir="/atlasgpfs01/usatlas/data/jroblesgo/benchmarks/$curr_time/EVNT"
     local  config_dir="EVNTFiles/100xxx/100001/"
     local  OScontainer="centos7"
