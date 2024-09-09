b0956907721895b20edd853482c29911ecf59aa3
Juan <selbor903@gmail.com>
2024-09-08 21:25:44 -0700

    evnt script changes

--- EVNT/run_EVNT.sh
+++ EVNT/run_EVNT.sh
@@ -25,10 +25,7 @@ Container(){
 Batch(){
   export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
   source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
-  pwd
-  ls
   asetup AthGeneration,23.6.31,here
-  pwd
   Gen_tf.py --ecmEnergy=13000.0 --jobConfig=${1}  --outputEVNTFile=EVNT.root --maxEvents=10000 --randomSeed=${2}
 }
 
