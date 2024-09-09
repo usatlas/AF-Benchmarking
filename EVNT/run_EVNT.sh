11d9d0e5e10738ac3f5104695c399e8f63d30748
Juan <selbor903@gmail.com>
2024-09-09 12:14:33 -0700

    Debugging

--- EVNT/run_EVNT.sh
+++ EVNT/run_EVNT.sh
@@ -15,7 +15,7 @@ EOF
 ## 3 -- Seed
 Container(){
   export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
-  source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${1} -r "asetup AthGeneration,23.6.31,here ; pwd; ls && \
+  source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${1} -r "asetup AthGeneration,23.6.31,here && \
   Gen_tf.py --ecmEnergy=13000.0 --jobConfig=${2}  --outputEVNTFile=EVNT.root --maxEvents=10000 --randomSeed=${3}"
 }
 
@@ -23,6 +23,8 @@ Container(){
 ## 1 -- config_dir
 ## 2 -- Seed
 Batch(){
+  pwd
+  ls
   export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
   source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
   pwd
@@ -52,7 +54,7 @@ main() {
     local  output_dir="/atlasgpfs01/usatlas/data/jroblesgo/benchmarks/$curr_time/EVNT"
     local  config_dir="EVNTFiles/100xxx/100001/"
     local  OScontainer="centos7"
-    Container ${OScontainer} ${config_dir} ${seed}
+    Batch ${config_dir} ${seed}
   elif [[ -d /data ]]
   then
     local  output_dir="/data/selbor/benchmarks/$curr_date/EVNT/"
