666be82c4eeab73603ff16258acd3f378bc96207
Juan <selbor903@gmail.com>
2024-09-09 11:09:20 -0700

    Added comments to the script

--- Rucio/rucio_script.sh
+++ Rucio/rucio_script.sh
@@ -1,17 +1,20 @@
 #! /bin/bash
 
+# Removes the downloads if they exist
 rm -r mc23_13p6TeV.700866.Sh_2214_WWW_3l3v_EW6.deriv.DAOD_PHYSLITE.e8532_e8528_s4162_s4114_r14622_r14663_p6026_tid37222410_00/
 
+# Setting up authorization for the download
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 export ALRB_localConfigDir=$HOME/localConfig
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
 lsetup emi
 echo "Wookiee13" | voms-proxy-init -voms atlas
-
 lsetup "rucio -w"
 
+# Gets the current time
 curr_time=$(date +"%Y.%m.%dT%H")
 
+# Conditional block determines the AF
 if [[ -d /sdf ]]; then
   output_dir="/sdf/data/atlas/u/$USER/benchmarks/$curr_time/Rucio/"
 elif [[ -d /usatlas ]]
@@ -22,9 +25,12 @@ then
   output_dir="/data/$USER/benchmarks/$curr_time/Rucio/"
 fi
 
+# Makes the dir for the log file (if it doesn't exist)
 mkdir -p ${output_dir}
-
+# Begins the download
 rucio download --rses AGLT2_LOCALGROUPDISK mc23_13p6TeV:mc23_13p6TeV.700866.Sh_2214_WWW_3l3v_EW6.deriv.DAOD_PHYSLITE.e8532_e8528_s4162_s4114_r14622_r14663_p6026_tid37222410_00 2>&1 | tee $output_dir/rucio.log
 
+# Gets the host name
 hostname >> $output_dir/rucio.log
+# Gets the download size
 du mc23_13p6TeV.700866.Sh_2214_WWW_3l3v_EW6.deriv.DAOD_PHYSLITE.e8532_e8528_s4162_s4114_r14622_r14663_p6026_tid37222410_00/ >> $output_dir/rucio.log
