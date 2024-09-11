ff18e25fb253edf886847125de22cb4c698ac897
Juan <selbor903@gmail.com>
2024-09-11 09:38:54 -0700

    Changes to the names of the outputdir for the truth3 job

--- TRUTH3/run_truth3.sh
+++ TRUTH3/run_truth3.sh
@@ -28,21 +28,21 @@ main() {
 
   # Checks for the home directory
   if [[ -d /sdf ]]; then
-    local  output_dir="/sdf/data/atlas/u/$USER/benchmarks/$curr_time/EVNT"
+    local  output_dir="/sdf/data/atlas/u/$USER/benchmarks/$curr_time/TRUTH3"
     local  config_dir=$HOME/evntFiles/100xxx/100001
     local  OScontainer="centos7"
     Container ${OScontainer} ${config_dir} ${seed}
   elif [[ -d /usatlas ]]
   then
     # There is a madgraph error; I can just raise a flag and have the process skip that step.
-    local  output_dir="/atlasgpfs01/usatlas/data/jroblesgo/benchmarks/$curr_time/EVNT"
+    local  output_dir="/atlasgpfs01/usatlas/data/jroblesgo/benchmarks/$curr_time/TRUTH3"
     local  config_dir="EVNTFiles/100xxx/100001/"
     local  OScontainer="centos7"
     cd /usatlas/workarea/jroblesgo
     Batch ${config_dir} ${seed}
   elif [[ -d /data ]]
   then
-    local  output_dir="/data/selbor/benchmarks/$curr_date/EVNT/"
+    local  output_dir="/data/selbor/benchmarks/$curr_date/TRUTH3/"
     local  config_dir="/data/selbor/evnt/100xxx/100001/"
     local  OScontainer="centos7"
     Batch ${config_dir} ${seed}
