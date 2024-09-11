9c8d7c7fcc090bab5e51b807b9ea6fee10c85509
Juan <selbor903@gmail.com>
2024-09-11 11:58:52 -0700

    Changed the OS used in the container

--- TRUTH3/run_truth3.sh
+++ TRUTH3/run_truth3.sh
@@ -37,7 +37,7 @@ main() {
     # There is a madgraph error; I can just raise a flag and have the process skip that step.
     local  output_dir="/atlasgpfs01/usatlas/data/jroblesgo/benchmarks/$curr_time/TRUTH3"
     local  config_dir="EVNTFiles/100xxx/100001/"
-    local  OScontainer="centos7"
+    local  OScontainer="el9"
     cd /usatlas/workarea/jroblesgo
     Container ${OScontainer} ${config_dir} ${seed}
   elif [[ -d /data ]]
