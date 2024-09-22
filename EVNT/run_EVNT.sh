a587b65218b7277db8035a4706683329da41c2d2
Juan <selbor903@gmail.com>
2024-09-21 17:37:00 -0700

    Updated the evnt script for bnl

--- EVNT/run_EVNT.sh
+++ EVNT/run_EVNT.sh
@@ -21,8 +21,8 @@ Container(){
 
 Container_bnl(){ 
   export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
-  source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${1} -r "cd ${2} && \ asetup AthGeneration,23.6.31,here && \
-  pwd Gen_tf.py --ecmEnergy=13000.0 --jobConfig=${2}  --outputEVNTFile=EVNT.root --maxEvents=10000 --randomSeed=${3}"
+  source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${1} -r "cd ${2} asetup AthGeneration,23.6.31,here && \
+  Gen_tf.py --ecmEnergy=13000.0 --jobConfig=${2}  --outputEVNTFile=EVNT.root --maxEvents=10000 --randomSeed=${3}"
 }
 
 # Takes in the following parameters:
