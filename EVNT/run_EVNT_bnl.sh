18309e82111d7e5c92f1a450a26bafb514c65f43
selbor <selbor903@gmail.com>
2024-09-24 16:02:37 -0700

    Changes made to the evnt script used at bnl

---
+++ EVNT/run_EVNT_bnl.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+# The seed used in the job
+seed=1001
+
+# Directory where the input files are stored
+config_dir="../EVNTFiles/100xxx/100001/"
+
+# The OS used in the container
+OScontainer="centos7"
+
+# Directory where the job will take place
+job_dir="/tmp/jroblesgo/EVNT_Job"
+
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OScontainer} -r "cd ${job_dir} &&\
+asetup AthGeneration,23.6.31,here &&\
+Gen_tf.py --ecmEnergy=13000.0 --jobConfig=${config_dir}  --outputEVNTFile=EVNT.root --maxEvents=10000 --randomSeed=${seed}"
+
+# Current time used for log file storage
+curr_time=$(date +"%Y.%m.%dT%H")
+
+# Output directory
+output_dir="/atlasgpfs01/usatlas/data/jroblesgo/benchmarks/$curr_time/EVNT"
+
+mkdir -p ${output_dir}
+mv log.generate ${output_dir}
+#hostname >> ${output_dir}/log.generate
+mv myjob.* ${output_dir}
+# I still need to get the payload size into the log file.
+
