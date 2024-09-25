a55f76e7eb8c94a4c76c66e50e77b91a2556819c
selbor <selbor903@gmail.com>
2024-09-25 11:16:41 -0700

    TRUTH3 Script used at slac

--- TRUTH3/run_truth3_slac.sh
+++ TRUTH3/run_truth3_slac.sh
@@ -3,6 +3,7 @@
 config_dir=$HOME/TRUTH3Files/
 OScontainer="el9"
 
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OScontainer} -r "asetup Athena,24.0.53,here && \
   Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT.root --outputDAODFile=TRUTH3.root --formats TRUTH3"
 
