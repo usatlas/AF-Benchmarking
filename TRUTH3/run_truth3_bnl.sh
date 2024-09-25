3866b46d5ac17385b46d1e54151ee2231a4dfb49
selbor <selbor903@gmail.com>
2024-09-24 19:45:45 -0700

    truth3 script used at bnl

---
+++ TRUTH3/run_truth3_bnl.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+OScontainer="el9"
+config_dir="TRUTH3Files/"
+
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OScontainer} -r "asetup Athena,24.0.53,here && \
+  Derivation_tf.py --CA True --inputEVNTFile ${2}EVNT.root --outputDAODFile=TRUTH3.root --formats TRUTH3"
+
+curr_time=$(date +"%Y.%m.%dT%H")
+# There is a madgraph error; I can just raise a flag and have the process skip that step.
+output_dir="/atlasgpfs01/usatlas/data/jroblesgo/benchmarks/$curr_time/TRUTH3"
+mkdir -p ${output_dir}
+hostname >> *.generate
+du TRUTH3.root >> *.generate
+mv myjob.* ${output_dir}
+mv log.* ${output_dir}
+ 
+
