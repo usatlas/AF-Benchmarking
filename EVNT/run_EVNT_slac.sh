5662b17449132979a7df3a07d8f9986fdbd2a2e0
selbor <selbor903@gmail.com>
2024-09-24 17:54:13 -0700

    Changed the evnt script used at slac

--- EVNT/run_EVNT_slac.sh
+++ EVNT/run_EVNT_slac.sh
@@ -10,7 +10,7 @@ OScontainer="centos7"
 seed=1001
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OScontainer} -r "asetup AthGeneration,23.6.31,here && \
-pwd Gen_tf.py --ecmEnergy=13000.0 --jobConfig=${config_dir}  --outputEVNTFile=EVNT.root --maxEvents=10000 --randomSeed=${seed}"
+Gen_tf.py --ecmEnergy=13000.0 --jobConfig=${config_dir}  --outputEVNTFile=EVNT.root --maxEvents=10000 --randomSeed=${seed}"
 
 # Current time used for log file storage
 curr_time=$(date +"%Y.%m.%dT%H")
