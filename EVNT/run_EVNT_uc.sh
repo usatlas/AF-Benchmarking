076787f7ffe07e9ab05a8e498dd335d5af4fa717
selbor <selbor903@gmail.com>
2024-09-26 11:34:39 -0700

    Script will now mount the data dir into the container

--- EVNT/run_EVNT_uc.sh
+++ EVNT/run_EVNT_uc.sh
@@ -32,4 +32,11 @@ then
   export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
   source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OS_container} -m /data:/data -r "asetup AthGeneration,23.6.34,here && \
   Gen_tf.py --ecmEnergy=13000.0 --jobConfig=${config_dir}  --outputEVNTFile=EVNT.root --maxEvents=10000 --randomSeed=${seed}"
+  
+  mkdir -p ${output_dir}
+  hostname >> log.generate
+  du EVNT.root >> log.generate
+  mv log.generate ${output_dir}
+  mv myjob.* ${output_dir}
+
 fi
