13780611ccf2619f94f6fc08c7bef88b4950609c
selbor <selbor903@gmail.com>
2024-09-26 14:10:29 -0700

    Changed the script

--- TRUTH3/run_truth3_uc.sh
+++ TRUTH3/run_truth3_uc.sh
@@ -13,21 +13,16 @@ then
   source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
   asetup Athena,24.0.53,here
   Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT.root --outputDAODFile=TRUTH3.root --formats TRUTH3
-  mkdir -p ${output_dir}
-  hostname >> log.generate
-  du TRUTH3.root >> log.generate
-  mv myjob.* ${output_dir}
-  mv log.generate ${output_dir}
 elif [[ ${1} == c ]]
 then
   export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
   source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c centos7 -m /data:/data - r " asetup Athena,24.0.53,here && \
     Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT.root --outputDAODFile=TRUTH3.root --formats TRUTH3"
-  
-  mkdir -p ${output_dir}
-  hostname >> log.generate
-  du TRUTH3.root >> log.generate
-  mv myjob.* ${output_dir}
-  mv log.generate ${output_dir}
 fi
+
+mkdir -p ${output_dir}
+hostname >> log.generate
+du DAOD_TRUTH3.TRUTH3.root >> log.generate
+mv myjob.* ${output_dir}
+mv log.generate ${output_dir}
 
