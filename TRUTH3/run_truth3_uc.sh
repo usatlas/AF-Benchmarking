7192b7edabf592eca7f3ce93ce694c861f29a7f4
selbor <selbor903@gmail.com>
2024-09-26 13:40:01 -0700

    Changes to uc script used for the truth3 job; can handle input, to be used for container version

--- TRUTH3/run_truth3_uc.sh
+++ TRUTH3/run_truth3_uc.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
-
-config_dir="/data/selbor/evnt/100xxx/100001/"
-
-export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
-source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
-asetup Athena,24.0.53,here
-Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT.root --outputDAODFile=TRUTH3.root --formats TRUTH3
-
 # Current time used for log file storage
 curr_time=$(date +"%Y.%m.%dT%H")
 
 output_dir="/data/selbor/benchmarks/$curr_date/TRUTH3/"
-mkdir -p ${output_dir}
-hostname >> *.generate
-du TRUTH3.root >> *.generate
-mv myjob.* ${output_dir}
-mv log.* ${output_dir}
- 
+
+config_dir="/data/selbor/evnt/100xxx/100001/"
+
+if [[ ${1} == n ]]
+then 
+  export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+  source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
+  asetup Athena,24.0.53,here
+  Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT.root --outputDAODFile=TRUTH3.root --formats TRUTH3
+  mkdir -p ${output_dir}
+  hostname >> log.generate
+  du TRUTH3.root >> log.generate
+  mv myjob.* ${output_dir}
+  mv log.generate ${output_dir}
+elif [[ ${1} == c ]]
+then
+  export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+  source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c centos7 -m /data:/data - r " asetup Athena,24.0.53,here && \
+    Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT.root --outputDAODFile=TRUTH3.root --formats TRUTH3"
+  
+  mkdir -p ${output_dir}
+  hostname >> log.generate
+  du TRUTH3.root >> log.generate
+  mv myjob.* ${output_dir}
+  mv log.generate ${output_dir}
+fi
+
