0b8693ff761081abb527d6da0f9e3de8c7b2f58c
selbor <selbor903@gmail.com>
2024-09-26 16:47:48 -0700

    Made it have its own separe interactive file

--- TRUTH3/run_truth3_uc.sh
+++ TRUTH3/run_truth3_uc.sh
@@ -18,7 +18,7 @@ then
   output_dir="/data/selbor/benchmarks/$curr_time/TRUTH3_centos_interactive/"
   export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
   source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c centos7 -m /data:/data - r " asetup Athena,24.0.53,here && \
-    Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT_centos.root --outputDAODFile=TRUTH3.root --formats TRUTH3"
+    Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT_centos_interactive.root --outputDAODFile=TRUTH3.root --formats TRUTH3"
 elif [[ ${1} == cb ]]
 then
   output_dir="/data/selbor/benchmarks/$curr_time/TRUTH3_centos/"
