978a3d5bb13929c85d486649c2096c0e407aa9ee
selbor <selbor903@gmail.com>
2024-09-26 16:00:12 -0700

    Changed the output directory to match the jobs

--- TRUTH3/run_truth3_uc.sh
+++ TRUTH3/run_truth3_uc.sh
@@ -3,20 +3,19 @@
 # Current time used for log file storage
 curr_time=$(date +"%Y.%m.%dT%H")
 
-output_dir="/data/selbor/benchmarks/$curr_time/TRUTH3/"
 
 config_dir="/data/selbor/TRUTH3_StaticDir/"
 
 if [[ ${1} == n ]]
 then
-  rm *
+  output_dir="/data/selbor/benchmarks/$curr_time/TRUTH3/"
   export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
   source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
   asetup Athena,24.0.53,here
   Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT.root --outputDAODFile=TRUTH3.root --formats TRUTH3
 elif [[ ${1} == c ]]
 then
-  rm *
+  output_dir="/data/selbor/benchmarks/$curr_time/TRUTH3_centos_interactive/"
   export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
   source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c centos7 -m /data:/data - r " asetup Athena,24.0.53,here && \
     Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT_centos.root --outputDAODFile=TRUTH3.root --formats TRUTH3"
