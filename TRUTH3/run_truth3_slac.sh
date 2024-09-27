1032bc54adc3ff7a5c4dcd040d5cb57650ec60be
selbor <selbor903@gmail.com>
2024-09-27 16:13:35 -0700

    Changed the name of the input file

--- TRUTH3/run_truth3_slac.sh
+++ TRUTH3/run_truth3_slac.sh
@@ -12,15 +12,15 @@ then
   curr_time=$(date +"%Y.%m.%dT%H")
   output_dir="/sdf/data/atlas/u/$USER/benchmarks/$curr_time/TRUTH3"
   mkdir -p ${output_dir}
-  hostname >> log.Derivation
-  du DAOD_TRUTH3.TRUTH3.root >> log.Derivation
+  hostname >> log.EVNTtoDAOD
+  du DAOD_TRUTH3.TRUTH3.root >> log.EVNTtoDAOD
 elif [[ ${1} == c ]]
 then
   config_dir="TRUTH3Files/"
   OScontainer="centos7"
   export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
   source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OScontainer} -r "asetup AthDerivation,21.2.178.0,here && \
-    Reco_tf.py --inputEVNTFile ${config_dir}EVNT_centos_interactive.root --outputDAODFile=TRUTH3.root --reductionConf TRUTH3"
+    Reco_tf.py --inputEVNTFile ${config_dir}EVNT.root --outputDAODFile=TRUTH3.root --reductionConf TRUTH3"
 
   curr_time=$(date +"%Y.%m.%dT%H")
   output_dir="/sdf/data/atlas/u/$USER/benchmarks/$curr_time/TRUTH3"
