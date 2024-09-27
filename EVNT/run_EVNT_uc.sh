810bf0b61d45dfd973cc631b7f609da39d27a1f2
selbor <selbor903@gmail.com>
2024-09-27 10:28:27 -0700

    Changes to the script

--- EVNT/run_EVNT_uc.sh
+++ EVNT/run_EVNT_uc.sh
@@ -9,11 +9,10 @@ then
   # Directory storing the input files
   config_dir="/data/selbor/evnt/100xxx/100001"
 
-
   export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
   source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
   asetup AthGeneration,23.6.34,here
-  Gen_tf.py --ecmEnergy=13000.0 --jobConfig=${config_dir}  --outputEVNTFile=EVNT.root --maxEvents=100 --randomSeed=${seed}
+  Gen_tf.py --ecmEnergy=13000.0 --jobConfig=${config_dir}  --outputEVNTFile=EVNT.root --maxEvents=10000 --randomSeed=${seed}
   # Current time used for log file storage
   curr_time=$(date +"%Y.%m.%dT%H")
   # Directory where all the output files will be sent to
@@ -29,19 +28,18 @@ then
   # UChicago uses the AthGeneration,23.6.34
   OS_container="el9"
 
-  # Current time used for log file storage
-  curr_time=$(date +"%Y.%m.%dT%H")
-
   # The seed used in the job
   seed=1001
 
   # Directory storing the input files
   config_dir="/data/selbor/evnt/100xxx/100001"
 
-
   export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
   source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OS_container} -m /data:/data -r "asetup AthGeneration,23.6.34,here && \
   Gen_tf.py --ecmEnergy=13000.0 --jobConfig=${config_dir}  --outputEVNTFile=EVNT.root --maxEvents=100 --randomSeed=${seed}"
+  
+  # Current time used for log file storage
+  curr_time=$(date +"%Y.%m.%dT%H")
   output_dir="/data/selbor/benchmarks/$curr_time/EVNT_contained/"
   mkdir -p ${output_dir}
   hostname >> log.generate
