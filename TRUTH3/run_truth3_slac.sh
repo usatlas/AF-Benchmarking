d555b02af9730fbdae2b94bdd5674ef3aac3900f
selbor <selbor903@gmail.com>
2024-09-27 15:29:46 -0700

    Changes to the script

--- TRUTH3/run_truth3_slac.sh
+++ TRUTH3/run_truth3_slac.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-config_dir=TRUTH3Files/
-OScontainer="el9"
 
-export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
-source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OScontainer} -r "asetup Athena,24.0.53,here && \
-  Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT.root --outputDAODFile=TRUTH3.root --formats TRUTH3"
+if [[ ${1} == n ]]
+then
+  config_dir="/sdf/data/atlas/u/selbor/TRUTH3Files"
+  OScontainer="el9"
+  export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+  source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OScontainer} -r "asetup Athena,24.0.53,here && \
+    Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT.root --outputDAODFile=TRUTH3.root --formats TRUTH3"
 
-curr_time=$(date +"%Y.%m.%dT%H")
-output_dir="/sdf/data/atlas/u/$USER/benchmarks/$curr_time/TRUTH3"
-mkdir -p ${output_dir}
-hostname >> log.generate
-du DAOD_TRUTH3.TRUTH3.root >> log.generate
-
-
+  curr_time=$(date +"%Y.%m.%dT%H")
+  output_dir="/sdf/data/atlas/u/$USER/benchmarks/$curr_time/TRUTH3"
+  mkdir -p ${output_dir}
+  hostname >> log.generate
+  du DAOD_TRUTH3.TRUTH3.root >> log.generate
+fi
 
