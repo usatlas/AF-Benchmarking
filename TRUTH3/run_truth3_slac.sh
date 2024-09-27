d5ca0fb4b9c73968acc3901de22cd6325a02e30e
selbor <selbor903@gmail.com>
2024-09-27 16:29:28 -0700

    added the native case

--- TRUTH3/run_truth3_slac.sh
+++ TRUTH3/run_truth3_slac.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-if [[ ${1} == n ]]
+if [[ ${1} == e ]]
 then
   config_dir="TRUTH3Files/"
   OScontainer="el9"
@@ -27,5 +27,20 @@ then
   mkdir -p ${output_dir}
   hostname >> log.Derivation
   du DAOD_TRUTH3.TRUTH3.root >> log.Derivation
+elif [[ ${1} == n ]]
+then
+  config_dir="TRUTH3Files/centos7/"
+  OScontainer="centos7"
+  export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+  source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
+  asetup AthDerivation,21.2.178.0,here
+  Reco_tf.py --inputEVNTFile ${config_dir}EVNT.root --outputDAODFile=TRUTH3.root --reductionConf TRUTH3
+
+  curr_time=$(date +"%Y.%m.%dT%H")
+  output_dir="/sdf/data/atlas/u/$USER/benchmarks/$curr_time/TRUTH3"
+  mkdir -p ${output_dir}
+  hostname >> log.Derivation
+  du DAOD_TRUTH3.TRUTH3.root >> log.Derivation
+
 fi
 
