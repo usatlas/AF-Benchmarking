cea840c4e6611d1aa24ad0ac11db0d01d59fb70a
selbor <selbor903@gmail.com>
2024-09-27 11:01:19 -0700

    Fixed a typo

--- TRUTH3/run_truth3_uc.sh
+++ TRUTH3/run_truth3_uc.sh
@@ -16,12 +16,11 @@ then
   du DAOD_TRUTH3.TRUTH3.root >> log.Derivation
   mv myjob.* ${output_dir}
   mv log.Derivation ${output_dir}
-
 elif [[ ${1} == i ]]
 then
   config_dir="/data/selbor/TRUTH3_StaticDir/"
   export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
-  source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c centos7 -m /data:/data - r "asetup Athena,24.0.53,here && \
+  source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c centos7 -m /data:/data -r "asetup Athena,24.0.53,here && \
     Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT_centos_interactive.root --outputDAODFile=TRUTH3.root --formats TRUTH3"
   # current time used for log file storage
   curr_time=$(date +"%Y.%m.%dT%H")
@@ -31,13 +30,12 @@ then
   du DAOD_TRUTH3.TRUTH3.root >> log.Derivation
   mv myjob.* ${output_dir}
   mv log.Derivation ${output_dir}
-
 elif [[ ${1} == c ]]
 then
   config_dir="/data/selbor/TRUTH3_StaticDir/"
   
   export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
-  source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c centos7 -m /data:/data - r " asetup Athena,24.0.53,here && \
+  source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c centos7 -m /data:/data -r " asetup Athena,24.0.53,here && \
     Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT_centos.root --outputDAODFile=TRUTH3.root --formats TRUTH3"
   # current time used for log file storage
   curr_time=$(date +"%Y.%m.%dT%H")
