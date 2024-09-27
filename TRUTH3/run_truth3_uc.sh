f165102faedde1225523bdc55fcd253ee82364c5
selbor <selbor903@gmail.com>
2024-09-27 10:48:07 -0700

    Changed the date formatting

--- TRUTH3/run_truth3_uc.sh
+++ TRUTH3/run_truth3_uc.sh
@@ -9,7 +9,7 @@ then
     asetup Athena,24.0.53,here
   Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT.root --outputDAODFile=TRUTH3.root --formats TRUTH3
   # current time used for log file storage
-  curr_time=$(date +"%y.%m.%dt%h")
+  curr_time=$(date +"%Y.%m.%dT%H")
   output_dir="/data/selbor/benchmarks/$curr_time/TRUTH3"
   mkdir -p ${output_dir}
   hostname >> log.Derivation
@@ -24,7 +24,7 @@ then
   source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c centos7 -m /data:/data - r " asetup Athena,24.0.53,here && \
     Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT_centos_interactive.root --outputDAODFile=TRUTH3.root --formats TRUTH3"
   # current time used for log file storage
-  curr_time=$(date +"%y.%m.%dt%h")
+  curr_time=$(date +"%Y.%m.%dT%H")
   output_dir="/data/selbor/benchmarks/$curr_time/TRUTH3_centos_interactive/"
   mkdir -p ${output_dir}
   hostname >> log.Derivation
@@ -40,7 +40,7 @@ then
   source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c centos7 -m /data:/data - r " asetup Athena,24.0.53,here && \
     Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT_centos.root --outputDAODFile=TRUTH3.root --formats TRUTH3"
   # current time used for log file storage
-  curr_time=$(date +"%y.%m.%dt%h")
+  curr_time=$(date +"%Y.%m.%dT%H")
   output_dir="/data/selbor/benchmarks/$curr_time/TRUTH3_centos/"
   mkdir -p ${output_dir}
   hostname >> log.Derivation
