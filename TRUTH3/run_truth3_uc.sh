b53780876ae1c583e041fa9e2d1458ae196c5abc
selbor <selbor903@gmail.com>
2024-09-27 10:43:54 -0700

    Changes to the script

--- TRUTH3/run_truth3_uc.sh
+++ TRUTH3/run_truth3_uc.sh
@@ -4,13 +4,13 @@
 if [[ ${1} == n ]]
 then
   config_dir="/data/selbor/TRUTH3_StaticDir/"
-  output_dir="/data/selbor/benchmarks/$curr_time/TRUTH3/"
   export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
   source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
     asetup Athena,24.0.53,here
   Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT.root --outputDAODFile=TRUTH3.root --formats TRUTH3
   # current time used for log file storage
   curr_time=$(date +"%y.%m.%dt%h")
+  output_dir="/data/selbor/benchmarks/$curr_time/TRUTH3"
   mkdir -p ${output_dir}
   hostname >> log.Derivation
   du DAOD_TRUTH3.TRUTH3.root >> log.Derivation
@@ -20,12 +20,12 @@ then
 elif [[ ${1} == ci ]]
 then
   config_dir="/data/selbor/TRUTH3_StaticDir/"
-  output_dir="/data/selbor/benchmarks/$curr_time/TRUTH3_centos_interactive/"
   export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
   source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c centos7 -m /data:/data - r " asetup Athena,24.0.53,here && \
     Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT_centos_interactive.root --outputDAODFile=TRUTH3.root --formats TRUTH3"
   # current time used for log file storage
   curr_time=$(date +"%y.%m.%dt%h")
+  output_dir="/data/selbor/benchmarks/$curr_time/TRUTH3_centos_interactive/"
   mkdir -p ${output_dir}
   hostname >> log.Derivation
   du DAOD_TRUTH3.TRUTH3.root >> log.Derivation
@@ -35,12 +35,13 @@ then
 elif [[ ${1} == cb ]]
 then
   config_dir="/data/selbor/TRUTH3_StaticDir/"
-  output_dir="/data/selbor/benchmarks/$curr_time/TRUTH3_centos/"
+  
   export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
   source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c centos7 -m /data:/data - r " asetup Athena,24.0.53,here && \
     Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT_centos.root --outputDAODFile=TRUTH3.root --formats TRUTH3"
   # current time used for log file storage
   curr_time=$(date +"%y.%m.%dt%h")
+  output_dir="/data/selbor/benchmarks/$curr_time/TRUTH3_centos/"
   mkdir -p ${output_dir}
   hostname >> log.Derivation
   du DAOD_TRUTH3.TRUTH3.root >> log.Derivation
