238a55c73f95f9f47aa230bc608d36a441ff3bee
selbor <selbor903@gmail.com>
2024-09-27 10:34:23 -0700

    Changes to the script

--- TRUTH3/run_truth3_uc.sh
+++ TRUTH3/run_truth3_uc.sh
@@ -1,36 +1,50 @@
 #!/bin/bash
 
-# Current time used for log file storage
-curr_time=$(date +"%Y.%m.%dT%H")
-
-
-config_dir="/data/selbor/TRUTH3_StaticDir/"
 
 if [[ ${1} == n ]]
 then
+  config_dir="/data/selbor/TRUTH3_StaticDir/"
   output_dir="/data/selbor/benchmarks/$curr_time/TRUTH3/"
   export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
   source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
-  asetup Athena,24.0.53,here
+    asetup Athena,24.0.53,here
   Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT.root --outputDAODFile=TRUTH3.root --formats TRUTH3
+  # current time used for log file storage
+  curr_time=$(date +"%y.%m.%dt%h")
+  mkdir -p ${output_dir}
+  hostname >> log.Derivation
+  du DAOD_TRUTH3.TRUTH3.root >> log.Derivation
+  mv myjob.* ${output_dir}
+  mv log.Derivation ${output_dir}
+
 elif [[ ${1} == ci ]]
 then
+  config_dir="/data/selbor/TRUTH3_StaticDir/"
   output_dir="/data/selbor/benchmarks/$curr_time/TRUTH3_centos_interactive/"
   export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
   source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c centos7 -m /data:/data - r " asetup Athena,24.0.53,here && \
     Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT_centos_interactive.root --outputDAODFile=TRUTH3.root --formats TRUTH3"
+  # current time used for log file storage
+  curr_time=$(date +"%y.%m.%dt%h")
+  mkdir -p ${output_dir}
+  hostname >> log.Derivation
+  du DAOD_TRUTH3.TRUTH3.root >> log.Derivation
+  mv myjob.* ${output_dir}
+  mv log.Derivation ${output_dir}
+
 elif [[ ${1} == cb ]]
 then
+  config_dir="/data/selbor/TRUTH3_StaticDir/"
   output_dir="/data/selbor/benchmarks/$curr_time/TRUTH3_centos/"
   export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
   source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c centos7 -m /data:/data - r " asetup Athena,24.0.53,here && \
     Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT_centos.root --outputDAODFile=TRUTH3.root --formats TRUTH3"
-
+  # current time used for log file storage
+  curr_time=$(date +"%y.%m.%dt%h")
+  mkdir -p ${output_dir}
+  hostname >> log.Derivation
+  du DAOD_TRUTH3.TRUTH3.root >> log.Derivation
+  mv myjob.* ${output_dir}
+  mv log.Derivation ${output_dir}
 fi
 
-mkdir -p ${output_dir}
-hostname >> log.Derivation
-du DAOD_TRUTH3.TRUTH3.root >> log.Derivation
-mv myjob.* ${output_dir}
-mv log.Derivation ${output_dir}
-cd ${job_dir} && rm *
