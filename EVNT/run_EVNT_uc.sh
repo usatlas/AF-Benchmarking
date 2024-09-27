d3394227b2f68069750cb36d408bf260de0b31d9
selbor <selbor903@gmail.com>
2024-09-27 10:16:18 -0700

    Changes to the script

--- EVNT/run_EVNT_uc.sh
+++ EVNT/run_EVNT_uc.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
-# UChicago uses the AthGeneration,23.6.34
-# Current time used for log file storage
-curr_time=$(date +"%Y.%m.%dT%H")
-
-# The seed used in the job
-seed=1001
-
-# Directory storing the input files
-config_dir="/data/selbor/evnt/100xxx/100001"
-
-# Directory where all the output files will be sent to
-output_dir="/data/selbor/benchmarks/$curr_date/EVNT/"
-
-OS_container="el9"
-
 # Native OS for UC is CentOS7
 if [[ ${1} == n ]]
 then
+  # Current time used for log file storage
+  curr_time=$(date +"%Y.%m.%dT%H")
+
+  # The seed used in the job
+  seed=1001
+
+  # Directory storing the input files
+  config_dir="/data/selbor/evnt/100xxx/100001"
+
+
   export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
   source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
   asetup AthGeneration,23.6.34,here
   Gen_tf.py --ecmEnergy=13000.0 --jobConfig=${config_dir}  --outputEVNTFile=EVNT.root --maxEvents=10000 --randomSeed=${seed}
+  # Directory where all the output files will be sent to
+  output_dir="/data/selbor/benchmarks/$curr_time/EVNT/"
+
 
   mkdir -p ${output_dir}
   hostname >> log.generate
@@ -30,14 +28,26 @@ then
   mv myjob.* ${output_dir}
 elif [[ ${1} == c ]]
 then
+  # UChicago uses the AthGeneration,23.6.34
+  OS_container="el9"
+
+  # Current time used for log file storage
+  curr_time=$(date +"%Y.%m.%dT%H")
+
+  # The seed used in the job
+  seed=1001
+
+  # Directory storing the input files
+  config_dir="/data/selbor/evnt/100xxx/100001"
+
+
   export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
   source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OS_container} -m /data:/data -r "asetup AthGeneration,23.6.34,here && \
   Gen_tf.py --ecmEnergy=13000.0 --jobConfig=${config_dir}  --outputEVNTFile=EVNT.root --maxEvents=100 --randomSeed=${seed}"
-  
+  output_dir="/data/selbor/benchmarks/$curr_time/EVNT_contained/"
   mkdir -p ${output_dir}
   hostname >> log.generate
   du EVNT.root >> log.generate
   mv log.generate ${output_dir}
   mv myjob.* ${output_dir}
-
 fi
