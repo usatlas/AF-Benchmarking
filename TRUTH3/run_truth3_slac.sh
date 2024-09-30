f36d0de036e75094cca8d9ed9c3eb8575b99b4ff
Juan <selbor903@gmail.com>
2024-09-30 14:46:48 -0700

    Changes to the truth3 script used at slac

--- TRUTH3/run_truth3_slac.sh
+++ TRUTH3/run_truth3_slac.sh
@@ -8,15 +8,14 @@ then
   export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
   source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OScontainer} -r "asetup Athena,24.0.53,here && \
     Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT.root --outputDAODFile=TRUTH3.root --formats TRUTH3"
-
   curr_time=$(date +"%Y.%m.%dT%H")
   output_dir="/sdf/data/atlas/u/$USER/benchmarks/$curr_time/TRUTH3"
   mkdir -p ${output_dir}
-  hostname >> log.EVNTtoDAOD
-  du DAOD_TRUTH3.TRUTH3.root >> log.EVNTtoDAOD
+  hostname >> log.Derivation
+  du DAOD_TRUTH3.TRUTH3.root >> log.Derivation
 elif [[ ${1} == c ]]
 then
-  config_dir="TRUTH3Files/centos7/"
+  config_dir="TRUTH3Files/"
   OScontainer="centos7"
   export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
   source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OScontainer} -r "asetup AthDerivation,21.2.178.0,here && \
@@ -30,7 +29,6 @@ then
 elif [[ ${1} == n ]]
 then
   config_dir="TRUTH3Files/"
-  OScontainer="centos7"
   export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
   source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
   asetup AthDerivation,21.2.178.0,here
