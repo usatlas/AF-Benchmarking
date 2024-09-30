8f7527064bca32f1940fd0dde0dd646b16468876
Juan <selbor903@gmail.com>
2024-09-30 14:58:01 -0700

    Changes to the truth3 script used at slac

--- TRUTH3/run_truth3_slac.sh
+++ TRUTH3/run_truth3_slac.sh
@@ -9,7 +9,7 @@ then
   source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OScontainer} -r "asetup Athena,24.0.53,here && \
     Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT.root --outputDAODFile=TRUTH3.root --formats TRUTH3"
   curr_time=$(date +"%Y.%m.%dT%H")
-  output_dir="/sdf/data/atlas/u/$USER/benchmarks/$curr_time/TRUTH3"
+  output_dir="/sdf/data/atlas/u/$USER/benchmarks/$curr_time/TRUTH3_el9_container"
   mkdir -p ${output_dir}
   hostname >> log.Derivation
   du DAOD_TRUTH3.TRUTH3.root >> log.Derivation
@@ -22,7 +22,7 @@ then
     Reco_tf.py --inputEVNTFile ${config_dir}EVNT.root --outputDAODFile=TRUTH3.root --reductionConf TRUTH3"
 
   curr_time=$(date +"%Y.%m.%dT%H")
-  output_dir="/sdf/data/atlas/u/$USER/benchmarks/$curr_time/TRUTH3"
+  output_dir="/sdf/data/atlas/u/$USER/benchmarks/$curr_time/TRUTH3_centos7_container"
   mkdir -p ${output_dir}
   hostname >> log.EVNTtoDAOD
   du DAOD_TRUTH3.TRUTH3.root >> log.EVNTtoDAOD
@@ -31,11 +31,11 @@ then
   config_dir="TRUTH3Files/"
   export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
   source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
-  asetup AthDerivation,21.2.178.0,here
-  Reco_tf.py --inputEVNTFile ${config_dir}EVNT.root --outputDAODFile=TRUTH3.root --reductionConf TRUTH3
+  asetup Athena,24.0.53,here
+  Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT.root --outputDAODFile=TRUTH3.root --formats TRUTH3
 
   curr_time=$(date +"%Y.%m.%dT%H")
-  output_dir="/sdf/data/atlas/u/$USER/benchmarks/$curr_time/TRUTH3"
+  output_dir="/sdf/data/atlas/u/$USER/benchmarks/$curr_time/TRUTH3_el9_native"
   mkdir -p ${output_dir}
   hostname >> log.Derivation
   du DAOD_TRUTH3.TRUTH3.root >> log.Derivation
