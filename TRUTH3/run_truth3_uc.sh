46e6063d2235a18c63aa47bc26cbe602545faea5
selbor <selbor903@gmail.com>
2024-10-01 11:48:06 -0700

    Made the script take allow el9 container

--- TRUTH3/run_truth3_uc.sh
+++ TRUTH3/run_truth3_uc.sh
@@ -44,5 +44,17 @@ then
   du DAOD_TRUTH3.TRUTH3.root >> log.Derivation
   mv myjob.* ${output_dir}
   mv log.Derivation ${output_dir}
+elif [[ ${1} == e ]]
+then
+  config_dir="/data/selbor/TRUTH3_StaticDir/"
+  OScontainer="el9"
+  export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+  source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OScontainer} -r "asetup Athena,24.0.53,here && \
+    Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT.root --outputDAODFile=TRUTH3.root --formats TRUTH3"
+  curr_time=$(date +"%Y.%m.%dT%H")
+  output_dir="/data/selbor/benchmarks/$curr_time/TRUTH3_el9_container"
+  mkdir -p ${output_dir}
+  hostname >> log.Derivation
+  du DAOD_TRUTH3.TRUTH3.root >> log.Derivation
 fi
 
