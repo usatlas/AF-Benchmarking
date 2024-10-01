7a5da09ebc1539e1f829faa8b787a3c1a807c471
Juan <selbor903@gmail.com>
2024-09-30 20:01:00 -0700

    submission file fot the slac af

---
+++ EVNT/benchmark_EVNT_slac_c_e.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+#SBATCH --account=atlas:usatlas
+#SBATCH --partition=ampere
+#SBATCH --gpus a100:1
+#SBATCH --job-name=evnt_batch
+#SBATCH --output=output-%j.txt
+#SBATCH --error=output-%j.txt
+#SBATCH --ntasks=1 
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=4g
+#SBATCH --time=0-02:00:00
+
+
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase 
+
+# Use ALRB_CONT_CMDOPTS to provide bind mount, etc. options 
+
+#export ALRB_CONT_CMDOPTS="-B /sdf"
+
+export ALRB_CONT_RUNPAYLOAD="source $HOME/AF-Benchmarking/EVNT/run_EVNT_slac.sh e"
+
+source $ATLAS_LOCAL_ROOT_BASE/user/atlasLocalSetup.sh -c centos7
