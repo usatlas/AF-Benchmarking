cfb26fe826b184a2aea27e264e4eb6b68728d8f2
Juan <selbor903@gmail.com>
2024-09-30 18:16:13 -0700

    Changed the evnt script used at slac

--- EVNT/run_EVNT_slac.sh
+++ EVNT/run_EVNT_slac.sh
@@ -1,24 +1,38 @@
 #!/bin/bash
 
-# Location of input files
-config_dir="$HOME/EVNTFiles/100xxx/100001"
+if [[ ${1}== c ]]
+  # Location of input files
+  config_dir="$HOME/EVNTFiles/100xxx/100001"
+  # OS used in the container
+  OScontainer="centos7"
+  # The seed used in the job
+  seed=1001
+  export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+  source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OScontainer} -r "asetup AthGeneration,23.6.31,here && \
+    Gen_tf.py --ecmEnergy=13000.0 --jobConfig=${config_dir}  --outputEVNTFile=EVNT.root --maxEvents=10000 --randomSeed=${seed}"
+      # Current time used for log file storage
+      curr_time=$(date +"%Y.%m.%dT%H")
+      output_dir="/sdf/data/atlas/u/$USER/benchmarks/$curr_time/EVNT_container_centos"
+elif [[ ${1} == e ]]
+then
+  # UChicago uses the AthGeneration,23.6.34
+  OS_container="el9"
+  # The seed used in the job
+  seed=1001
+  # Directory storing the input files
+  config_dir="/data/selbor/evnt/100xxx/100001"
+  export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+  source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OS_container} -m /data:/data -r "asetup AthGeneration,23.6.34,here && \
+  Gen_tf.py --ecmEnergy=13000.0 --jobConfig=${config_dir}  --outputEVNTFile=EVNT.root --maxEvents=100 --randomSeed=${seed}"
+  # Current time used for log file storage
+  curr_time=$(date +"%Y.%m.%dT%H")
+  output_dir="/data/selbor/benchmarks/$curr_time/EVNT_container_el/"
 
-# OS used in the container
-OScontainer="centos7"
-
-# The seed used in the job
-seed=1001
-export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
-source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OScontainer} -r "asetup AthGeneration,23.6.31,here && \
-Gen_tf.py --ecmEnergy=13000.0 --jobConfig=${config_dir}  --outputEVNTFile=EVNT.root --maxEvents=10000 --randomSeed=${seed}"
-
-# Current time used for log file storage
-curr_time=$(date +"%Y.%m.%dT%H")
-
-output_dir="/sdf/data/atlas/u/$USER/benchmarks/$curr_time/EVNT"
+fi
 
 mkdir -p ${output_dir}
-mv log.* ${output_dir}
-#hostname >> ${output_dir}/log.generate
+hostname >> log.generate
+du EVNT.root >> log.generate
+mv log.generate ${output_dir}
 mv myjob.* ${output_dir}
 
