4132881ff82f403513c5764222b38cdd45e5d705
selbor <jprobles@ucsc.edu>
2024-10-01 21:40:50 -0700

    Additions and changes to the SLAC TRUTH3 Job

--- TRUTH3/run_truth3_slac.sh
+++ TRUTH3/run_truth3_slac.sh
@@ -13,6 +13,7 @@ then
   mkdir -p ${output_dir}
   hostname >> log.Derivation
   du DAOD_TRUTH3.TRUTH3.root >> log.Derivation
+  mv log.Derivation ${output_dir}
 elif [[ ${1} == c ]]
 then
   config_dir="TRUTH3Files/"
@@ -26,21 +27,6 @@ then
   mkdir -p ${output_dir}
   hostname >> log.EVNTtoDAOD
   du DAOD_TRUTH3.TRUTH3.root >> log.EVNTtoDAOD
-elif [[ ${1} == n ]]
-then
-  config_dir="TRUTH3Files/"
-  export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
-  source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
-  asetup Athena,24.0.53,here
-  Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT.root --outputDAODFile=TRUTH3.root --formats TRUTH3
-
-  curr_time=$(date +"%Y.%m.%dT%H")
-  output_dir="/sdf/data/atlas/u/$USER/benchmarks/$curr_time/TRUTH3_el9_native"
-  mkdir -p ${output_dir}
-  hostname >> log.Derivation
-  du DAOD_TRUTH3.TRUTH3.root >> log.Derivation
-elif [[ ${1} == h ]]
-then
-cat /etc/os-release
+  mv log.EVNTtoDAOD ${output_dir}
 fi
 
---
+++ TRUTH3/script_TRUTH_slac_c_c.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /sdf/scratch/users/s/selbor/TRUTH3Job/container_centos
+rm *
+sbatch ~/AF-Benchmarking/TRUTH3/benchmark_TRUTH_slac_c_c.sh
