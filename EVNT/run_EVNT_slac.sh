b8ddd4b2f804d24c070d057f16437fdcd4a6a544
selbor <jprobles@ucsc.edu>
2024-10-01 20:59:46 -0700

    Changes to the evnt set up used at SLAC

--- EVNT/run_EVNT_slac.sh
+++ EVNT/run_EVNT_slac.sh
@@ -14,6 +14,10 @@ then
       # Current time used for log file storage
       curr_time=$(date +"%Y.%m.%dT%H")
       output_dir="/sdf/data/atlas/u/$USER/benchmarks/$curr_time/EVNT_container_centos"
+      mkdir -p ${output_dir}
+      hostname >> log.generate
+      du EVNT.root >> log.generate
+      mv log.generate ${output_dir}
 elif [[ ${1} == e ]]
 then
   # UChicago uses the AthGeneration,23.6.34
@@ -25,14 +29,13 @@ then
   export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
   source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OS_container} -r "asetup AthGeneration,23.6.34,here && \
   Gen_tf.py --ecmEnergy=13000.0 --jobConfig=${config_dir}  --outputEVNTFile=EVNT.root --maxEvents=100 --randomSeed=${seed}"
-  # Current time used for log file storage
-  curr_time=$(date +"%Y.%m.%dT%H")
-  output_dir="/data/selbor/benchmarks/$curr_time/EVNT_container_el/"
-
+    # Current time used for log file storage
+    curr_time=$(date +"%Y.%m.%dT%H")
+    output_dir="/data/selbor/benchmarks/$curr_time/EVNT_container_el/"
+    mkdir -p ${output_dir}
+    hostname >> log.generate
+    du EVNT.root >> log.generate
+    mv log.generate ${output_dir}
 fi
 
-mkdir -p ${output_dir}
-hostname >> log.generate
-du EVNT.root >> log.generate
-mv log.generate ${output_dir}
 
