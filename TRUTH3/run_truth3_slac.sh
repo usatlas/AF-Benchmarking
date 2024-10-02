4b8a6cf1045af932889a6b9a1067f1349eb07c02
selbor <selbor903@gmail.com>
2024-10-02 16:21:53 -0700

    minor changes

--- TRUTH3/run_truth3_slac.sh
+++ TRUTH3/run_truth3_slac.sh
@@ -13,7 +13,7 @@ then
     Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT.root --outputDAODFile=TRUTH3.root --formats TRUTH3" 2>&1 | tee $HOME/truth3.${curr_time}.txt
   echo "The container is done" >> $HOME/truth3.${curr_time}.txt
   curr_time=$(date +"%Y.%m.%dT%H")
-  output_dir="/sdf/data/atlas/u/selbor/benchmarks/$curr_time/TRUTH3_el9_container"
+  output_dir="/sdf/scratch/users/s/selbor/benchmarks/$curr_time/TRUTH3_el9_container"
   mkdir -p ${output_dir}
   hostname >> log.Derivation
   du DAOD_TRUTH3.TRUTH3.root >> log.Derivation
@@ -27,7 +27,7 @@ then
     Reco_tf.py --inputEVNTFile ${config_dir}EVNT.root --outputDAODFile=TRUTH3.root --reductionConf TRUTH3"
 
   curr_time=$(date +"%Y.%m.%dT%H")
-  output_dir="/sdf/data/atlas/u/$USER/benchmarks/$curr_time/TRUTH3_centos7_container"
+  output_dir="/sdf/scratch/users/s/selbor/benchmarks/$curr_time/TRUTH3_centos7_container"
   mkdir -p ${output_dir}
   hostname >> log.EVNTtoDAOD
   du DAOD_TRUTH3.TRUTH3.root >> log.EVNTtoDAOD
