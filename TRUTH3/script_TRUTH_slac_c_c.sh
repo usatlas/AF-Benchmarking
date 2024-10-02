2ac05aa3590fbffe3d24b001ded5974cf2c53301
selbor <selbor903@gmail.com>
2024-10-02 16:09:51 -0700

    changes to the dir

--- TRUTH3/script_TRUTH_slac_c_c.sh
+++ TRUTH3/script_TRUTH_slac_c_c.sh
@@ -3,4 +3,4 @@
 ssh iana
 cd /sdf/scratch/users/s/selbor/TRUTH3Job/container_centos
 rm *
-sbatch ~/AF-Benchmarking/TRUTH3/benchmark_TRUTH_slac_c_c.sh
+sbatch /sdf/home/s/selbor/AF-Benchmarking/TRUTH3/benchmark_TRUTH_slac_c_c.sh
