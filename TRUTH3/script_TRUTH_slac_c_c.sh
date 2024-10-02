fa37b5238dde5b11b93405a60dd222e8f8c88b73
selbor <selbor903@gmail.com>
2024-10-02 15:56:50 -0700

    ssh into the interactive cluster

--- TRUTH3/script_TRUTH_slac_c_c.sh
+++ TRUTH3/script_TRUTH_slac_c_c.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+ssh iana
 cd /sdf/scratch/users/s/selbor/TRUTH3Job/container_centos
 rm *
 sbatch ~/AF-Benchmarking/TRUTH3/benchmark_TRUTH_slac_c_c.sh
--- TRUTH3/script_TRUTH_slac_i_c.sh
+++ TRUTH3/script_TRUTH_slac_i_c.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+ssh iana
 cd /sdf/scratch/users/s/selbor/TRUTH3Job/interactive_centos
 rm *
 ~/AF-Benchmarking/TRUTH3/run_truth3_slac.sh c
