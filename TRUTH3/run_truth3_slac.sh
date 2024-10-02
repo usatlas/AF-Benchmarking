99b82a47b79a34b22a11c4173a979a4596883e0a
selbor <selbor903@gmail.com>
2024-10-02 13:06:59 -0700

    Changes to the script

--- TRUTH3/run_truth3_slac.sh
+++ TRUTH3/run_truth3_slac.sh
@@ -10,7 +10,7 @@ then
   export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
   echo "The container is started" >> $HOME/truth3.${curr_time}.txt
   source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OScontainer} -r "asetup Athena,24.0.53,here && \
-    Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT.root --outputDAODFile=TRUTH3.root --formats TRUTH3"
+    Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT.root --outputDAODFile=TRUTH3.root --formats TRUTH3" 2>&1 | tee $HOME/truth3.${curr_time}.txt
   echo "The container is done" >> $HOME/truth3.${curr_time}.txt
   curr_time=$(date +"%Y.%m.%dT%H")
   output_dir="/sdf/data/atlas/u/selbor/benchmarks/$curr_time/TRUTH3_el9_container"
