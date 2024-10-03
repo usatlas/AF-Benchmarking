1becbca7dbc97fd75cd286edbf820bdbb2f47f62
Juan <selbor903@gmail.com>
2024-10-03 14:49:02 -0700

    Changes to the script

--- TRUTH3/script_TRUTH_slac_c_e.sh
+++ TRUTH3/script_TRUTH_slac_c_e.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
-cd /sdf/scratch/users/s/selbor/TRUTH3Job/container_el
-rm *
+ssh iana "
+cd /sdf/scratch/users/s/selbor/TRUTH3Job/container_el &&
+rm * &&
 sbatch ~/AF-Benchmarking/TRUTH3/benchmark_TRUTH_slac_c_e.sh
+"
