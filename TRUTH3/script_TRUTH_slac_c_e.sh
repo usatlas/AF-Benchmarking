18aaa3f505474bf69a6482fe9cdada947a24cfd6
Juan <selbor903@gmail.com>
2024-10-03 14:02:56 -0700

    changed the directory it cds into

--- TRUTH3/script_TRUTH_slac_c_e.sh
+++ TRUTH3/script_TRUTH_slac_c_e.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /sdf/scratch/atlas/selbor/TRUTH3Job/container_el
+cd /sdf/scratch/users/s/selbor/TRUTH3Job/container_el
 rm *
 sbatch ~/AF-Benchmarking/TRUTH3/benchmark_TRUTH_slac_c_e.sh
