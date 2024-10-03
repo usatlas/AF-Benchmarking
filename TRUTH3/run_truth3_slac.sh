416fb5aaafde7d642cd01cfbc4af4de6d8b8beca
Juan <selbor903@gmail.com>
2024-10-03 13:55:59 -0700

    Changes to the script

--- TRUTH3/run_truth3_slac.sh
+++ TRUTH3/run_truth3_slac.sh
@@ -7,10 +7,12 @@ if [[ ${1} == e ]]
 then
   config_dir="TRUTH3Files/"
   OScontainer="el9"
-  export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
-  echo "The container is started" >> $HOME/truth3.${curr_time}.txt
-  source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OScontainer} -r "ls /cvmfs/atlas.cern.ch/repo/sw/software/24.0/ && asetup Athena,24.0.53,here && \
-    Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT.root --outputDAODFile=TRUTH3.root --formats TRUTH3" 2>&1 | tee $HOME/truth3.${curr_time}.txt
+  #export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+  #echo "The container is started" >> $HOME/truth3.${curr_time}.txt
+  #source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OScontainer} -r "
+  ls /cvmfs/atlas.cern.ch/repo/sw/software/24.0/
+  asetup Athena,24.0.53,here
+  Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT.root --outputDAODFile=TRUTH3.root --formats TRUTH3 2>&1 | tee $HOME/truth3.${curr_time}.txt
   echo "The container is done" >> $HOME/truth3.${curr_time}.txt
   curr_time=$(date +"%Y.%m.%dT%H")
   output_dir="/sdf/scratch/users/s/selbor/benchmarks/$curr_time/TRUTH3_el9_container"
