4569af041cdb82030766115bc64343b90124509a
Juan <selbor903@gmail.com>
2024-10-04 12:36:28 -0700

    changed the directory used at slac

--- TRUTH3/script_TRUTH_slac_c_c.sh
+++ TRUTH3/script_TRUTH_slac_c_c.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-ssh iana
-cd /sdf/scratch/users/s/selbor/TRUTH3Job/container_centos
+cd /sdf/scratch/users/s/selbor/benchmarks/TRUTH3Job/container_centos
 rm *
 sbatch /sdf/home/s/selbor/AF-Benchmarking/TRUTH3/benchmark_TRUTH_slac_c_c.sh
