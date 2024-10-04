603afe4d69800bd8d150511375879c5aa145608c
Juan <selbor903@gmail.com>
2024-10-04 12:10:21 -0700

    Changes to the script

--- TRUTH3/run_truth3_slac.sh
+++ TRUTH3/run_truth3_slac.sh
@@ -9,15 +9,15 @@ then
   OScontainer="el9"
   export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
   echo "The container is started" >> $HOME/truth3.${curr_time}.txt
-  source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OScontainer} -r "ls /cvmfs/atlas.cern.ch/repo/sw/software/24.0/ && asetup Athena,24.0.53,here && \
+  source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OScontainer} -r "asetup Athena,24.0.53,here && \
   Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT.root --outputDAODFile=TRUTH3.root --formats TRUTH3 2>&1 | tee $HOME/truth3.${curr_time}.txt"
   echo "The container is done" >> $HOME/truth3.${curr_time}.txt
   curr_time=$(date +"%Y.%m.%dT%H")
   output_dir="/sdf/scratch/users/s/selbor/benchmarks/$curr_time/TRUTH3_el9_container"
   mkdir -p ${output_dir}
-  hostname >> log.Derivation
-  du DAOD_TRUTH3.TRUTH3.root >> log.Derivation
-  mv log.Derivation ${output_dir}
+  hostname >> log.EVNTtoDAOD
+  du DAOD_TRUTH3.TRUTH3.root >> log.EVNTtoDAOD
+  mv log.EVNTtoDAOD ${output_dir}
 elif [[ ${1} == c ]]
 then
   config_dir="TRUTH3Files/"
@@ -33,4 +33,3 @@ then
   du DAOD_TRUTH3.TRUTH3.root >> log.EVNTtoDAOD
   mv log.EVNTtoDAOD ${output_dir}
 fi
-
