44f31e3342be195180a6a382b1c33b30634b04f3
Juan <selbor903@gmail.com>
2024-10-04 12:36:14 -0700

    changed the directory used at slac

--- EVNT/script_EVNT_slac_e.sh
+++ EVNT/script_EVNT_slac_e.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /sdf/scratch/users/s/selbor/EVNTJob/container_centos
+cd /sdf/scratch/users/s/selbor/benchmarks/EVNTJob/container_el
 rm *
 sbatch ~/AF-Benchmarking/EVNT/benchmark_EVNT_slac_c_e.sh
