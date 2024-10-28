8b9f3251a6e0bd57896dd04bd12782832b714178
Juan <selbor903@gmail.com>
2024-10-28 14:25:18 -0700

    Added the files for the interactive versions

---
+++ TRUTH3/SLAC/cron_truth3_centos7_interactive.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /sdf/scratch/users/s/selbor/TRUTH3Job/container_centos_interactive
+
+rm *
+
+/sdf/home/s/selbor/AF-Benchmarking/TRUTH3/SLAC/run_truth3_centos7_container_interactive.sh
---
+++ TRUTH3/SLAC/cron_truth3_el9_interactive.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /sdf/scratch/users/s/selbor/TRUTH3Job/container_el_interactive
+
+rm *
+
+/sdf/home/s/selbor/AF-Benchmarking/TRUTH3/SLAC/run_truth3_el9_interactive.sh
