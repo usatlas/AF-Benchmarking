5258ce96bd35b978e4af8ec9f6143288a306eb7a
Juan <selbor903@gmail.com>
2024-10-28 15:47:24 -0700

    fixed an error

--- TRUTH3/SLAC/cron_truth3_el9_interactive.sh
+++ TRUTH3/SLAC/cron_truth3_el9_interactive.sh
@@ -4,4 +4,4 @@ cd /sdf/scratch/users/s/selbor/TRUTH3Job/container_el_interactive
 
 rm *
 
-/sdf/home/s/selbor/AF-Benchmarking/TRUTH3/SLAC/run_truth3_el9_interactive.sh
+/sdf/home/s/selbor/AF-Benchmarking/TRUTH3/SLAC/run_truth3_el9_container_interactive.sh
