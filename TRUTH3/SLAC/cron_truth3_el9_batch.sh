2e53c3179d087049d99e13ff8b044f8bd314c06e
Juan <selbor903@gmail.com>
2024-10-28 14:24:39 -0700

    Changed the way the dir for executable was written

--- TRUTH3/SLAC/cron_truth3_el9_batch.sh
+++ TRUTH3/SLAC/cron_truth3_el9_batch.sh
@@ -4,4 +4,4 @@ cd /sdf/scratch/users/s/selbor/TRUTH3Job/container_el
 
 rm *
 
-sbatch ~/AF-Benchmarking/TRUTH3/SLAC/truth3_el9.sh
+sbatch /sdf/home/s/selbor/AF-Benchmarking/TRUTH3/SLAC/truth3_el9.sh
