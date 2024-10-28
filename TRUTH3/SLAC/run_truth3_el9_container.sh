a6e4d722dc798e4166f0ca1b10e07c916dd23867
Juan <selbor903@gmail.com>
2024-10-28 14:02:32 -0700

    Changed the name of input files

--- TRUTH3/SLAC/run_truth3_el9_container.sh
+++ TRUTH3/SLAC/run_truth3_el9_container.sh
@@ -6,9 +6,8 @@ curr_time=$(date +"%Y.%m.%dT%H")
 config_dir="TRUTH3Files/"
 OScontainer="el9"
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
-echo "The container is started" >> $HOME/truth3.${curr_time}.txt
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OScontainer} -r "asetup Athena,24.0.53,here && \
-Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT.root --outputDAODFile=TRUTH3.root --formats TRUTH3 2>&1 | tee $HOME/truth3.${curr_time}.txt"
+Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT_el9.root --outputDAODFile=TRUTH3.root --formats TRUTH3"
 
 output_dir="/sdf/scratch/users/s/selbor/benchmarks/$curr_time/TRUTH3_el9_container"
 mkdir -p ${output_dir}
