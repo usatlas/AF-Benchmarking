06c41f8d925a8baa373f6c9bbb051dc283793e34
Juan <selbor903@gmail.com>
2024-10-29 10:26:44 -0700

    Changed the output dir

--- TRUTH3/SLAC/run_truth3_centos7_container.sh
+++ TRUTH3/SLAC/run_truth3_centos7_container.sh
@@ -10,7 +10,7 @@ source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OScontainer} -r "as
   Reco_tf.py --inputEVNTFile ${config_dir}EVNT.root --outputDAODFile=TRUTH3.root --reductionConf TRUTH3"
 
 
-output_dir="/sdf/scratch/users/s/selbor/benchmarks/$curr_time/TRUTH3_centos7_container"
+output_dir="/home/selbor/benchmarks/$curr_time/TRUTH3_centos7_container"
 mkdir -p ${output_dir}
 hostname >> log.EVNTtoDAOD
 du DAOD_TRUTH3.TRUTH3.root >> log.EVNTtoDAOD
--- TRUTH3/SLAC/run_truth3_el9_container_interactive.sh
+++ TRUTH3/SLAC/run_truth3_el9_container_interactive.sh
@@ -9,7 +9,7 @@ export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OScontainer} -r "asetup Athena,24.0.53,here && \
 Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT.root --outputDAODFile=TRUTH3.root --formats TRUTH3"
 
-output_dir="/sdf/scratch/users/s/selbor/benchmarks/$curr_time/TRUTH3_el9_container_interactive"
+output_dir="/home/selbor/benchmarks/$curr_time/TRUTH3_el9_container_interactive"
 mkdir -p ${output_dir}
 hostname >> log.Derivation
 du DAOD_TRUTH3.TRUTH3.root >> log.Derivation
