0cdb10d5869dd45e1616fe5f020d839d559b6e75
Juan <selbor903@gmail.com>
2024-10-29 12:52:32 -0700

    Changed the config dir and commands used within the container

--- TRUTH3/SLAC/run_truth3_centos7_container.sh
+++ TRUTH3/SLAC/run_truth3_centos7_container.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 
-curr_time=$(date +"%Y.%m.%dT%H")
 
 config_dir="/home/selbor/TRUTH3Files/centos7/"
 OScontainer="centos7"
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
-source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OScontainer} -r "asetup AthDerivation,21.2.178.0,here && \
-  Reco_tf.py --inputEVNTFile ${config_dir}EVNT.root --outputDAODFile=TRUTH3.root --reductionConf TRUTH3"
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OScontainer} -r "cp -r /home/selbor/TRUTH3Files/ . && \
+asetup AthDerivation,21.2.178.0,here && \
+  Reco_tf.py --inputEVNTFile TRUTH3Files/centos7/EVNT.root --outputDAODFile=TRUTH3.root --reductionConf TRUTH3"
 
 
-output_dir="/home/selbor/benchmarks/$curr_time/TRUTH3_centos7_container"
+curr_time=$(date +"%Y.%m.%dT%H")
+output_dir="/home/selbor/benchmarks/${curr_time}/TRUTH3_centos7_container"
 mkdir -p ${output_dir}
 hostname >> log.EVNTtoDAOD
 du DAOD_TRUTH3.TRUTH3.root >> log.EVNTtoDAOD
--- TRUTH3/SLAC/run_truth3_centos7_container_interactive.sh
+++ TRUTH3/SLAC/run_truth3_centos7_container_interactive.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-
-curr_time=$(date +"%Y.%m.%dT%H")
-
-config_dir="/home/selbor/TRUTH3Files/centos7_interactive/"
 OScontainer="centos7"
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
-source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OScontainer} -r "asetup AthDerivation,21.2.178.0,here && \
-  Reco_tf.py --inputEVNTFile ${config_dir}EVNT.root --outputDAODFile=TRUTH3.root --reductionConf TRUTH3"
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OScontainer} -r "cp -r /home/selbor/TRUTH3Files/ . && \
+asetup AthDerivation,21.2.178.0,here && \
+  Reco_tf.py --inputEVNTFile /srv/TRUTH3Files/centos7_interactive/EVNT.root --outputDAODFile=TRUTH3.root --reductionConf TRUTH3"
 
 
-output_dir="/home/selbor/benchmarks/$curr_time/TRUTH3_centos7_container_interactive"
+curr_time=$(date +"%Y.%m.%dT%H")
+output_dir="/home/selbor/benchmarks/${curr_time}/TRUTH3_centos7_container_interactive"
 mkdir -p ${output_dir}
 hostname >> log.EVNTtoDAOD
 du DAOD_TRUTH3.TRUTH3.root >> log.EVNTtoDAOD
