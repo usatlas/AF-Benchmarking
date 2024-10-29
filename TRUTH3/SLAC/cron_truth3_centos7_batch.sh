4a369da9c95e3756822f78291667c03f92848fc1
Juan <selbor903@gmail.com>
2024-10-29 11:40:18 -0700

    Updated the files

--- TRUTH3/SLAC/cron_truth3_centos7_batch.sh
+++ TRUTH3/SLAC/cron_truth3_centos7_batch.sh
@@ -4,4 +4,4 @@ cd /sdf/scratch/users/s/selbor/TRUTH3Job/container_centos
 
 rm *
 
-sbatch /sdf/home/s/selbor/AF-Benchmarking/TRUTH3/SLAC/truth3_centos7.sh
+sbatch /sdf/home/s/selbor/AF-Benchmarking/TRUTH3/SLAC/truth3_centos7_sub.sh
