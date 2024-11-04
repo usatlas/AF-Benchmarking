4d506653f955107c38cd47c71e6e6a80f55dec8d
Juan <selbor903@gmail.com>
2024-11-04 12:16:51 -0800

    Fixed a typo

--- TRUTH3/UC/cron_truth3_el9_batch.sh
+++ TRUTH3/UC/cron_truth3_el9_batch.sh
@@ -4,4 +4,4 @@ cd /data/selbor/TRUTH3Job/container_el/
 
 rm *
 
-condor_submit /home/selbor/AF-Benchmarking/TRUTH3/UC/truth3_el.sub
+condor_submit /home/selbor/AF-Benchmarking/TRUTH3/UC/truth3_el9.sub
