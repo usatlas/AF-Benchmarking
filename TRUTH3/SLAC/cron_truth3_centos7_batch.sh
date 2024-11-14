24d2c6af85d6f5e9daf4aa7cba4cba804744d0ae
Juan <selbor903@gmail.com>
2024-11-14 15:23:21 -0800

    Removed a line

--- TRUTH3/SLAC/cron_truth3_centos7_batch.sh
+++ TRUTH3/SLAC/cron_truth3_centos7_batch.sh
@@ -5,6 +5,4 @@ cd /sdf/scratch/users/s/selbor/TRUTH3Job/container_centos
 
 rm -r *
 
-which sbatch 2>&1 | tee $HOME/testing_cron.txt
-
 sbatch /sdf/home/s/selbor/AF-Benchmarking/TRUTH3/SLAC/truth3_centos7_sub.sh"
