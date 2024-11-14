5e4e768eec344993752471ad7057626e696a3b19
Juan <selbor903@gmail.com>
2024-11-14 15:31:20 -0800

    Changed sbatch to srun; testing differences

--- TRUTH3/SLAC/cron_truth3_el9_batch.sh
+++ TRUTH3/SLAC/cron_truth3_el9_batch.sh
@@ -6,4 +6,4 @@ cd /sdf/scratch/users/s/selbor/TRUTH3Job/container_el
 
 rm -r *
 
-sbatch /sdf/home/s/selbor/AF-Benchmarking/TRUTH3/SLAC/truth3_el9_sub.sh"
+srun /sdf/home/s/selbor/AF-Benchmarking/TRUTH3/SLAC/truth3_el9_sub.sh"
