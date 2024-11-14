5f02388e93eca24fc6c9c4f4641b235f220fc0f9
Juan <selbor903@gmail.com>
2024-11-14 15:25:56 -0800

    Adding ssh iana to the scripts; makes them run with crontab

--- EVNT/SLAC/cron_evnt_centos7.sh
+++ EVNT/SLAC/cron_evnt_centos7.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+ssh iana"
 cd /sdf/scratch/users/s/selbor/EVNTJob/container_centos
 rm *
-sbatch /sdf/home/s/selbor/AF-Benchmarking/EVNT/SLAC/run_evnt_centos_container.sh
+sbatch /sdf/home/s/selbor/AF-Benchmarking/EVNT/SLAC/run_evnt_centos_container.sh"
