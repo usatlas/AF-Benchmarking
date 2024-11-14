6358a9ea7ccfb9d761f8988652dab5c16c64928b
Juan <selbor903@gmail.com>
2024-11-14 15:49:05 -0800

    Changed the script to match the tested script

--- TRUTH3/SLAC/cron_truth3_el9_batch.sh
+++ TRUTH3/SLAC/cron_truth3_el9_batch.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-ssh iana"
-
+ssh iana "
 cd /sdf/scratch/users/s/selbor/TRUTH3Job/container_el
 
 rm -r *
 
-srun /sdf/home/s/selbor/AF-Benchmarking/TRUTH3/SLAC/run_truth3_el9_container.sh"
+sbatch /sdf/home/s/selbor/AF-Benchmarking/TRUTH3/SLAC/truth3_el9_sub.sh"
