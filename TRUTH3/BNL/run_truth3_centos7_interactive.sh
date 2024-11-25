949e81e25138bdc70c3eb100952b42c1f6ce997e
Juan <selbor903@gmail.com>
2024-11-25 15:11:39 -0800

    Scripts used at BNL

---
+++ TRUTH3/BNL/run_truth3_centos7_interactive.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Config Dir Needed
+config_dir="TRUTH3Files/"
+
+# Sets up the ATLAS Environment
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+
+# Sets up the container:
+## -c : used to make a container followed by the OS we want to use
+## -m : mounts a specific directory
+## -r : precedes the commands we want to run within the container
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c centos7 -r "asetup AthDerivation,21.2.178.0,here && \
+  Reco_tf.py --inputEVNTFile ${config_dir}EVNT_centos_interactive.root --outputDAODFile=TRUTH3.root --reductionConf TRUTH3"
+
+# current time used for log file storage
+curr_time=$(date +"%Y.%m.%dT%H")
+
+# There is a madgraph error; I can just raise a flag and have the process skip that step.
+output_dir="/atlasgpfs01/usatlas/data/jroblesgo/benchmarks/$curr_time/TRUTH3_centos7_interactive"
+mkdir -p ${output_dir}
+hostname >> log.EVNTtoDAOD
+du DAOD_TRUTH3.TRUTH3.root >> log.EVNTtoDAOD
+# Moves the log file to the output directory
+mv log.EVNTtoDAOD ${output_dir}
---
+++ TRUTH3/BNL/run_truth3_el9_batch.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Config Dir Needed
+config_dir="TRUTH3Files/"
+
+# Sets up the environment
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+
+# Sets up the container:
+## -c : used to make a container followed by the OS we want to use
+## -m : mounts a specific directory
+## -r : precedes the commands we want to run within the container
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -r "asetup Athena,24.0.53,here && \
+  Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT_el9_batch.root --outputDAODFile=TRUTH3.root --formats TRUTH3"
+
+# Current time used for file storage
+curr_time=$(date +"%Y.%m.%dT%H")
+
+# Defines the output directory
+output_dir="/atlasgpfs01/usatlas/data/jroblesgo/benchmarks/$curr_time/TRUTH3_el9_batch"
+
+# Creates the output directory
+mkdir -p ${output_dir}
+
+# Obtains and appends the host name and payload size to the log file
+hostname >> log.Derivation
+du DAOD_TRUTH3.TRUTH3.root >> log.Derivation
+
+# Moves the log file to the output directory
+mv log.Derivation ${output_dir}
