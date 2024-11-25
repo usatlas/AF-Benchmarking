93054b2df81b5b71d5c0847f44ba00157be39e70
Juan <selbor903@gmail.com>
2024-11-25 15:53:04 -0800

    Adding dir for BNLs' evnt job

---
+++ EVNT/BNL/run_evnt_el9_interactive.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+# The seed used in the job
+seed=1001
+
+# Directory where the input files are stored
+config_dir="EVNTFiles/100xxx/100001/"
+
+# The OS used in the container
+OScontainer="el9"
+
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OScontainer} -r "cp -r ~/AF-Benchmarking/EVNT/EVNTFiles . &&\
+  asetup AthGeneration,23.6.31,here &&\
+  Gen_tf.py --ecmEnergy=13000.0 --jobConfig=${config_dir}  --outputEVNTFile=EVNT.root --maxEvents=100 --randomSeed=${seed}"
+
+# Current time used for log file storage
+curr_time=$(date +"%Y.%m.%dT%H")
+
+# Output directory
+output_dir="/atlasgpfs01/usatlas/data/jroblesgo/benchmarks/$curr_time/EVNT_el9_interactive"
+
+mkdir -p ${output_dir}
+hostname >> ${output_dir}/log.generate
+mv myjob.* ${output_dir}
+hostname >> log.generate
+du EVNT.root >> log.generate
+mv log.generate ${output_dir}
