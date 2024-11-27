d277d28290beddf931b969891dcd300d40e3707c
Juan <selbor903@gmail.com>
2024-11-27 07:06:30 -0800

    Testing the directories used within batch

--- TRUTH3/BNL/run_truth3_centos7_batch.sh
+++ TRUTH3/BNL/run_truth3_centos7_batch.sh
@@ -10,9 +10,10 @@ export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 ## -c : used to make a container followed by the OS we want to use
 ## -m : mounts a specific directory
 ## -r : precedes the commands we want to run within the container
-source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c centos7 -r "cp -r $HOME/TRUTH3Files . && \
-  asetup AthDerivation,21.2.178.0,here && \
-  Reco_tf.py --inputEVNTFile TRUTH3Files/EVNT_centos.root --outputDAODFile=TRUTH3.root --reductionConf TRUTH3"
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c centos7 -r "pwd && \ ls && \ cd $HOME && \ ls"
+#&& \ cp -r $HOME/TRUTH3Files . && \
+#  asetup AthDerivation,21.2.178.0,here && \
+#  Reco_tf.py --inputEVNTFile TRUTH3Files/EVNT_centos.root --outputDAODFile=TRUTH3.root --reductionConf TRUTH3"
 
 # current time used for log file storage
 curr_time=$(date +"%Y.%m.%dT%H")
