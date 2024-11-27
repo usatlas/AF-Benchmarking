7d7ec9cfb1c9244cefe3ce51e33c4b91b80ca2dc
Juan <selbor903@gmail.com>
2024-11-27 08:18:16 -0800

    Cleaned the EVNT dir

--- EVNT/script_EVNT_uc_c.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /data/selbor/EVNTJob/container_centos
-rm *
-condor_submit ~/AF-Benchmarking/EVNT/benchmark_EVNT_uc_c.sub
--- EVNT/script_EVNT_uc_e.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /data/selbor/EVNTJob/container_el
-rm *
-condor_submit ~/AF-Benchmarking/EVNT/benchmark_EVNT_uc_e.sub
