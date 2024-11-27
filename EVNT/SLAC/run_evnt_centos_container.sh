2fee26057eb1e8a9c37c919ae6efa35cbe25ff78
Juan <selbor903@gmail.com>
2024-11-27 14:04:13 -0800

    modified:   run_evnt_centos7_batch.sh 	deleted:    run_evnt_centos7_interactive.sh 	deleted:    run_evnt_el9_interactive.sh 	modified:   ../SLAC/run_evnt_centos_container.sh 	modified:   ../UC/run_evnt_centos7_batch.sh

--- EVNT/SLAC/run_evnt_centos_container.sh
+++ EVNT/SLAC/run_evnt_centos_container.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-# Location of input files
-config_dir="EVNTFiles/100xxx/100001/"
-# OS used in the container
-OScontainer="centos7"
-# The seed used in the job
-seed=1001
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
-source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OScontainer} -r "cp -r ~/EVNTFiles/ . && \
-asetup AthGeneration,23.6.31,here && \
-Gen_tf.py --ecmEnergy=13000.0 --jobConfig=${config_dir}  --outputEVNTFile=EVNT.root --maxEvents=10000 --randomSeed=${seed}"
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c centos7 -r "cp -r ~/AF-Benchmarking/EVNT/EVNTFiles . && asetup AthGeneration,23.6.31,here && export LHAPATH=/cvmfs/sft.cern.ch/lcg/external/lhapdfsets/current:/cvmfs/atlas.cern.ch/repo/sw/software/23.6/sw/lcg/releases/LCG_104d_ATLAS_13/MCGenerators/lhapdf/6.5.3/x86_64-centos7-gcc11-opt/share/LHAPDF:/cvmfs/atlas.cern.ch/repo/sw/Generators/lhapdfsets/current && export LHAPDF_DATA_PATH=/cvmfs/sft.cern.ch/lcg/external/lhapdfsets/current:/cvmfs/atlas.cern.ch/repo/sw/software/23.6/sw/lcg/releases/LCG_104d_ATLAS_13/MCGenerators/lhapdf/6.5.3/x86_64-centos7-gcc11-opt/share/LHAPDF:/cvmfs/atlas.cern.ch/repo/sw/Generators/lhapdfsets/current && Gen_tf.py --ecmEnergy=13000.0 --jobConfig=EVNTFiles/100xxx/100001/ --outputEVNTFile=EVNT.root --maxEvents=100 --randomSeed=1001"
+
+
 # Current time used for log file storage
 curr_time=$(date +"%Y.%m.%dT%H")
 output_dir="/home/selbor/benchmarks/$curr_time/EVNT_container_centos"
