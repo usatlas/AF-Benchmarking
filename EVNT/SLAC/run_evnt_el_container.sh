fc4edd2b63e47bf95bfe91b428c35a3d08f84f15
Juan <selbor903@gmail.com>
2024-12-04 08:27:30 -0800

    Adding comments to the evnt scripts used at slac

--- EVNT/SLAC/run_evnt_el_container.sh
+++ EVNT/SLAC/run_evnt_el_container.sh
@@ -8,6 +8,11 @@ seed=1001
 
 # Directory storing the input files
 config_dir="EVNTFiles/100xxx/100001/"
+
+# Sets up the container:
+## -c : used to make a container followed by the OS we want to use
+## -m : mounts a specific directory
+## -r : precedes the commands we want to run within the container
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OS_container} -r "cp -r ~/EVNTFiles/ . && \
 asetup AthGeneration,23.6.34,here && \
@@ -15,8 +20,12 @@ Gen_tf.py --ecmEnergy=13000.0 --jobConfig=${config_dir}  --outputEVNTFile=EVNT.r
 
 # Current time used for log file storage
 curr_time=$(date +"%Y.%m.%dT%H")
+# Defines the output directory
 output_dir="/home/selbor/benchmarks/$curr_time/EVNT_container_el/"
+# Creates the output directory
 mkdir -p ${output_dir}
+# Obtains and appends the host name and payload size to the log file
 hostname >> log.generate
 du EVNT.root >> log.generate
+# Moves the log file to the output directory
 mv log.generate ${output_dir}
