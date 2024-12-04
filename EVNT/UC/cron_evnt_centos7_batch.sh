20b1a256023b8ff9775ad80df5c77d09021d7b22
Juan <selbor903@gmail.com>
2024-12-03 16:57:52 -0800

    Fixing a bug

--- EVNT/UC/cron_evnt_centos7_batch.sh
+++ EVNT/UC/cron_evnt_centos7_batch.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd /data/selbor/EVNTJob/container_centos
 
-rm *
+rm -r *
+
+cp /home/selbor/AF-Benchmarking/EVNT/EVNTFiles/ .
 
 condor_submit /home/selbor/AF-Benchmarking/EVNT/UC/evnt_centos7.sub
