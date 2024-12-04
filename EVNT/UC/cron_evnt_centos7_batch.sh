f652e3b9163a95c07b7b94f841f4207f06f8cc0f
Juan <selbor903@gmail.com>
2024-12-03 17:01:13 -0800

    Fixed a typo

--- EVNT/UC/cron_evnt_centos7_batch.sh
+++ EVNT/UC/cron_evnt_centos7_batch.sh
@@ -3,6 +3,6 @@ cd /data/selbor/EVNTJob/container_centos
 
 rm -r *
 
-cp /home/selbor/AF-Benchmarking/EVNT/EVNTFiles/ .
+cp /home/selbor/AF-Benchmarking/EVNT/EVNTFiles .
 
 condor_submit /home/selbor/AF-Benchmarking/EVNT/UC/evnt_centos7.sub
