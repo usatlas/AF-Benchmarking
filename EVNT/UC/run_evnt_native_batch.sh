378b9dbd3981add4f843e1f46a322298c09420cb
Juan <selbor903@gmail.com>
2024-12-11 08:49:57 -0800

    removed my user name and replaced it with selbor

--- EVNT/UC/run_evnt_native_batch.sh
+++ EVNT/UC/run_evnt_native_batch.sh
@@ -4,7 +4,7 @@
 seed=1001
 
 # Directory storing the input files
-config_dir="/data/selbor/evnt/100xxx/100001"
+config_dir="/data/$USER/evnt/100xxx/100001"
 
 # Sets up our working environment
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
@@ -18,7 +18,7 @@ Gen_tf.py --ecmEnergy=13000.0 --jobConfig=${config_dir}  --outputEVNTFile=EVNT.r
 curr_time=$(date +"%Y.%m.%dT%H")
 
 # Directory where all the output files will be sent to
-output_dir="/data/selbor/benchmarks/${curr_time}/EVNT/"
+output_dir="/data/$USER/benchmarks/${curr_time}/EVNT/"
 
 # Makes the output directory
 mkdir -p ${output_dir}
