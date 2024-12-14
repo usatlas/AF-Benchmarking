21743eb3cbffebdc58f5beea4ae782f72816cb80
Juan <selbor903@gmail.com>
2024-12-13 19:57:38 -0800

    Added the rest of the script for el9 batch for NERSC

--- TRUTH3/NERSC/run_truth3_el9_batch.sh
+++ TRUTH3/NERSC/run_truth3_el9_batch.sh
@@ -15,3 +15,19 @@ source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OScontainer} -r "as
   Derivation_tf.py --CA True --inputEVNTFile EVNT.root --outputDAODFile=TRUTH3.root --formats TRUTH3"
 
 rm EVNT.root
+
+# Current time used for file storage
+curr_time=$(date +"%Y.%m.%dT%H")
+
+# Defines the output directory
+output_dir="$HOME/benchmarks/$curr_time/TRUTH3_el9_container"
+
+# Creates the output directory
+mkdir -p ${output_dir}
+
+# Obtains and appends the host name and payload size to the log file
+hostname >> log.Derivation
+du DAOD_TRUTH3.TRUTH3.root >> log.Derivation
+
+# Moves the log file to the output directory
+mv log.Derivation ${output_dir}
