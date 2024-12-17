55cd87491927c648784603578ce5e5bde41b27c9
Juan <selbor903@gmail.com>
2024-12-17 09:34:18 -0800

    Testing script at NERSC

--- Rucio/rucio_script.sh
+++ Rucio/rucio_script.sh
@@ -7,9 +7,9 @@ container_el9 (){
   # - download_ID (3)
   export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
   export ALRB_localConfigDir=$HOME/localConfig
-  source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -r "lsetup emi && \
+  source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -r "export RUCIO_ACCOUNT=jroblesg && \
+    lsetup rucio &&\
     echo Wookiee13 | voms-proxy-init -voms atlas && \
-    lsetup rucio -w &&\
     mkdir -p ${1} &&\
     cd ${2} &&\
     rm -r ${3}/ &&\
