0dfd031537ed6aa844f277a7649e13916f95cf48
Juan <selbor903@gmail.com>
2024-12-17 09:48:43 -0800

    Testing script at NERSC

--- Rucio/rucio_script.sh
+++ Rucio/rucio_script.sh
@@ -9,7 +9,7 @@ container_el9 (){
   export ALRB_localConfigDir=$HOME/localConfig
   source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -r "export RUCIO_ACCOUNT=jroblesg && \
     lsetup rucio &&\
-    echo Wookiee13 | voms-proxy-init -voms atlas && \
+    cat $HOME/pass.txt | voms-proxy-init -voms atlas && \
     mkdir -p ${1} &&\
     cd ${2} &&\
     rm -r ${3}/ &&\
