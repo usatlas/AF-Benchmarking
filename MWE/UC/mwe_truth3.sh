79fdef640235ced2966d9d8b0b3e34bb990800cb
Juan <selbor903@gmail.com>
2025-01-06 12:17:55 -0800

    mwe_truth3.sh

--- MWE/UC/mwe_truth3.sh
+++ MWE/UC/mwe_truth3.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+# Moves to the data directory
+cd /data/$USER/MWE/
+
 # Prints the directory the job is started at
 pwd 2>&1 | tee mwe.log
 
 # Prints the items at the directory
 ls >> mwe.log
+
+cp /data/$USER/TRUTH3_StaticDir/EVNT.root .
+
+# Sets up our environment
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
+
+# Sets the Athena version we want
+asetup Athena,24.0.53,here
+Derivation_tf.py --CA True --inputEVNTFile EVNT.root --outputDAODFile=TRUTH3.root --formats TRUTH3
+
