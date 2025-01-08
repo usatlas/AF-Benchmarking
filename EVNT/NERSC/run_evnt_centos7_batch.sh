4ef8836c603323b751918387a6ffc42cc06a3a8d
Juan <selbor903@gmail.com>
2025-01-08 11:13:54 -0800

    run_evnt_*

--- EVNT/NERSC/run_evnt_centos7_batch.sh
+++ EVNT/NERSC/run_evnt_centos7_batch.sh
@@ -16,6 +16,6 @@ export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 ## -m : mounts a specific directory
 ## -r : precedes the commands we want to run within the container
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OS_container} -r "asetup AthGeneration,23.6.31,here && \
-Gen_tf.py --ecmEnergy=13000.0 --jobConfig=${config_dir}  --outputEVNTFile=EVNT.root --maxEvents=100 --randomSeed=${seed}"
+Gen_tf.py --ecmEnergy=13000.0 --jobConfig=${config_dir}  --outputEVNTFile=EVNT.root --maxEvents=10000 --randomSeed=${seed}"
 
 rm -r evnt_centos/
