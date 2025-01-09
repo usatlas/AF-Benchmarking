c11f0d8a0ee436a5109bf6dc506153d7fa8b76f3
Juan <selbor903@gmail.com>
2025-01-09 09:31:25 -0800

    run_evnt_*

--- EVNT/NERSC/run_evnt_el9_batch.sh
+++ EVNT/NERSC/run_evnt_el9_batch.sh
@@ -22,3 +22,19 @@ source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OS_container} -r "a
 Gen_tf.py --ecmEnergy=13000.0 --jobConfig=${config_dir}  --outputEVNTFile=EVNT.root --maxEvents=10000 --randomSeed=${seed}"
 
 rm -r evnt_el9/
+
+# Current time used for log file storage
+curr_time=$(date +"%Y.%m.%dT%H")
+
+# Defines and makes the output directory
+output_dir="$HOME/benchmarks/$curr_time/EVNT_el9/"
+mkdir -p ${output_dir}
+
+# Obtains and appends the host name and payload size to the log file
+hostname >> log.generate
+du EVNT.root >> log.generate
+
+# Moves the log file to the output directory
+mv log.generate ${output_dir}
+
+rm *
