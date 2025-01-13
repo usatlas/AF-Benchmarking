441bc3249927ec726071ee46ca8bd16990b201e7
Juan <selbor903@gmail.com>
2025-01-13 14:19:09 -0800

    TRUTH3/UC/example_run_truth3_native_batch.sh

--- TRUTH3/UC/example_run_truth3_native_batch.sh
+++ TRUTH3/UC/example_run_truth3_native_batch.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# Input files are stored here
 # Input/large files should always be stored in the /data/$USER directory
 inputFile_dir="/data/$(whoami)/TRUTH3_Native_input_file/"
 
-mkdir -p ${config_dir}
+# Creates the input file directory defined above
+mkdir -p ${inputFile_dir}
 
-cp ~/AF-Benchmarking/TRUTH3/EVNT.root ${config_dir}
+# Copies the input files to the designated directory
+cp ~/AF-Benchmarking/TRUTH3/EVNT.root ${inputFile_dir}
 
 # Sets up our environment
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
@@ -14,8 +15,9 @@ source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
 
 # Sets the Athena version we want
 asetup Athena,24.0.53,here
-Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT.root --outputDAODFile=TRUTH3.root --formats TRUTH3
+Derivation_tf.py --CA True --inputEVNTFile ${inputFile_dir}EVNT.root --outputDAODFile=TRUTH3.root --formats TRUTH3
 
-# Obtains and appends the host machine and payload size to the log file
+# Obtains and appends the host machine to the log file
 hostname >> log.Derivation
+
 
