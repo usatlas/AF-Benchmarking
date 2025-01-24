a93a54555080ddb3e27c4eaf45028b7df03cac33
Juan <selbor903@gmail.com>
2025-01-24 09:55:05 -0800

    Script that downloads input files for coffea wf; ntuple-to-hist

---
+++ NTuple_Hist/coffea/coffea_input_file_download.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Blocks sets up rucio downloads
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+export ALRB_localConfigDir=$HOME/localConfig
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
+lsetup emi
+cat $HOME/pass.txt | voms-proxy-init -voms atlas
+lsetup "rucio -w"
+
+# Lists all files gstark has available for download
+# Any v2 file should work
+#rucio list-rules --account gstark
+
+rucio download --rses MWT2_UC_LOCALGROUPDISK user.bhodkins:user.bhodkins.data2018_AllYear.v2.0_ANALYSIS.root
