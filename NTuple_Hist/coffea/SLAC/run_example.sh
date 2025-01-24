3ed0e4aff9cbb05d3ca66031a4860317cdbd85f8
Juan <selbor903@gmail.com>
2025-01-24 12:05:26 -0800

    Adding the script that will run the ntuple to hist, using coffea, job at SLAC

---
+++ NTuple_Hist/coffea/SLAC/run_example.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+
+# Gets the current time
+curr_time=$(date +"%Y.%m.%dT%H")
+
+# Working directory
+work_dir="/sdf/scratch/users/s/selbor/Ntuple_Hist/coffea"
+
+cd ${work_dir}
+
+source ../bin/activate
+
+start_time=$date
+
+$start_time >> coffea_hist.log
+
+python3 example.py 2>&1 | tee coffea_hist.log
+
+end_time=$date
+
+$end_time >> coffea_hist.log
+
+hostname >> coffea_hist.log
+
+du ntuple_cfw.pdf >> coffea_hist.log
+
+rm ntuple_cfw.pdf
+
+log_file_dir="/sdf/home/s/selbor/benchmarks/${curr_time}/Coffea_Hist/"
+
+mkdir -p ${log_file_dir}
+
+mv coffea_hist.log ${log_file_dir}
