f62817680fe7908bffb841fc89e1d20d70715bc4
Juan <selbor903@gmail.com>
2025-01-24 14:35:19 -0800

    example.py

--- NTuple_Hist/coffea/NERSC/example.py
+++ NTuple_Hist/coffea/NERSC/example.py
@@ -55,7 +55,7 @@ if __name__ == "__main__":
 
     client = Client()
 
-    fname = "/sdf/scratch/users/s/selbor/Ntuple_Hist/coffea/user.bhodkins.data2018_AllYear.v2.0_ANALYSIS.root/user.bhodkins.42164748._000288.ANALYSIS.root"
+    fname = "/pscratch/sd/s/selbor/ntuple/coffea/user.bhodkins.data2018_AllYear.v2.0_ANALYSIS.root/user.bhodkins.42164748._000288.ANALYSIS.root"
     events = NanoEventsFactory.from_root(
         {fname: "analysis"},
         schemaclass=NtupleSchema,
