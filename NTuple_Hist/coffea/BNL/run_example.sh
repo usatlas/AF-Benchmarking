0e0a0981a625fd97ac8616357f72e045750b0ab5
Juan <selbor903@gmail.com>
2025-01-29 10:02:21 -0800

    Coffea script for BNL now runs in a container

--- NTuple_Hist/coffea/BNL/run_example.sh
+++ NTuple_Hist/coffea/BNL/run_example.sh
@@ -6,9 +6,7 @@ cd /atlasgpfs01/usatlas/scratch/jroblesgo/ntuple/coffea
 
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 
-source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c centos7 -r "export ALRB_pythonVersion=3.9.21-x86_64-centos7 &&\
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c centos7 -r "lsetup 'python 3.9.21-x86_64-centos7' &&\
 
-  lsetup root &&\
-
-  python3 /usatlas/u/jroblesgo/AF-Benchmarking/NTuple_Hist/coffea/BNL/example.py
+  python3 ./AF-Benchmarking/NTuple_Hist/coffea/BNL/example.py
 "
