b4118f6fea9970362df2887b4f0522b33f259447
Juan <selbor903@gmail.com>
2025-01-29 09:54:07 -0800

    run_example.sh

--- NTuple_Hist/coffea/BNL/run_example.sh
+++ NTuple_Hist/coffea/BNL/run_example.sh
@@ -6,7 +6,9 @@ cd /atlasgpfs01/usatlas/scratch/jroblesgo/ntuple/coffea
 
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 
-source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c centos7 -r "lsetup python 3.9.21-x86_64-centos7 &&\
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c centos7 -r "export ALRB_pythonVersion=3.9.21-x86_64-centos7 &&\
 
-python3 /usatlas/u/jroblesgo/AF-Benchmarking/NTuple_Hist/coffea/BNL/example.py
+  lsetup root &&\
+
+  python3 /usatlas/u/jroblesgo/AF-Benchmarking/NTuple_Hist/coffea/BNL/example.py
 "
