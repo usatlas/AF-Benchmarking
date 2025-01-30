b18d5cf674c620f1dacedae6b9128d09934073d2
Juan <selbor903@gmail.com>
2025-01-29 16:35:31 -0800

    run_ff.sh

--- NTuple_Hist/fastframes/UC/run_ff.sh
+++ NTuple_Hist/fastframes/UC/run_ff.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-setupATLAS
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+export ALRB_localConfigDir=$HOME/localConfig
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
+ 
 # Sets up root and boost
 asetup StatAnalysis,0.5.0
 
