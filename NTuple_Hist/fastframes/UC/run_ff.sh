10fcbe1bd594f707a3cc1ed34d3b2d08465b3b92
Juan <selbor903@gmail.com>
2025-01-29 16:28:34 -0800

    fastframes dir

---
+++ NTuple_Hist/fastframes/UC/run_ff.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+setupATLAS
+# Sets up root and boost
+asetup StatAnalysis,0.5.0
+
+# Sets up rucio
+cat $HOME/pass.txt | voms-proxy-init -voms atlas
+lsetup rucio -w
+
+
+# Set output path as $HOME due to permission issues
+cat $HOME/ff_local_GD.txt | python3 produce_metadata_files.py --grid_datasets ../../input/rucio_input.txt  --sum_weights_histo user.bhodkins:user.bhodkins.700402.Wmunugamma.mc20a.v2.0_hist --output_path $HOME/
