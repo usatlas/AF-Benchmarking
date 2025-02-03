9f91f2c31ab8de60c3a43f2a1a8bcd3d2e6a7c03
juan <jprobles@ucsc.edu>
2025-02-03 11:45:54 -0800

    run_example.sh

--- NTuple_Hist/coffea/NERSC/run_example.sh
+++ NTuple_Hist/coffea/NERSC/run_example.sh
@@ -2,11 +2,11 @@
 
 # Run this in a container
 
-cd #Job Directory used
+cd /pscratch/sd/s/selbor/ntuple/coffea
 
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c centos7 -r "lsetup 'python 3.9.21-x86_64-centos7' &&\
 
-  python3 ./AF-Benchmarking/NTuple_Hist/coffea/BNL/example.py
+  python3 ./AF-Benchmarking/NTuple_Hist/coffea/NERSC/example.py
 "
