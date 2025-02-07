46a4e92f6612393c94d76e8ae0c0983831cb7eb7
Juan <selbor903@gmail.com>
2025-02-07 11:18:33 -0800

    Updated the directories used

--- TRUTH3/SLAC/cron_truth3_centos7_batch.sh
+++ TRUTH3/SLAC/cron_truth3_centos7_batch.sh
@@ -5,4 +5,4 @@ cd /sdf/scratch/users/s/selbor/TRUTH3Job/container_centos
 
 rm -r *
 
-sbatch /sdf/home/s/selbor/AF-Benchmarking/TRUTH3/SLAC/truth3_centos7_sub.sh"
+sbatch /sdf/data/atlas/u/selbor/AF-Benchmarking/TRUTH3/SLAC/truth3_centos7_sub.sh"
