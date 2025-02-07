b278f328a6d266783eff3ec946ef33331731380b
Juan <selbor903@gmail.com>
2025-02-07 11:18:33 -0800

    Updated the directories used

--- TRUTH3/SLAC/cron_truth3_el9_batch.sh
+++ TRUTH3/SLAC/cron_truth3_el9_batch.sh
@@ -5,4 +5,4 @@ cd /sdf/scratch/users/s/selbor/TRUTH3Job/container_el
 
 rm -r *
 
-sbatch /sdf/home/s/selbor/AF-Benchmarking/TRUTH3/SLAC/truth3_el9_sub.sh"
+sbatch /sdf/data/atlas/u/selbor/AF-Benchmarking/TRUTH3/SLAC/truth3_el9_sub.sh"
