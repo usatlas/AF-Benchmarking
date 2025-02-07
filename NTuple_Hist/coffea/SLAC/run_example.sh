4fba113ef0c5c97fbd7ef182a32de4432c1f88cb
Juan <selbor903@gmail.com>
2025-02-07 13:55:35 -0800

    updated the script to match the one used at BNL

--- NTuple_Hist/coffea/SLAC/run_example.sh
+++ NTuple_Hist/coffea/SLAC/run_example.sh
@@ -9,13 +9,16 @@ work_dir="/sdf/data/atlas/u/selbor/Ntuple_Hist/coffea"
 
 cd ${work_dir}
 
-source ../bin/activate
-
 start_time=$date
 
 $start_time >> coffea_hist.log
 
-python3 example.py 2>&1 | tee coffea_hist.log
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c centos7 -r "lsetup 'python 3.9.21-x86_64-centos7' &&\
+
+  python3 ./AF-Benchmarking/NTuple_Hist/coffea/BNL/example.py
+"
 
 end_time=$date
 
