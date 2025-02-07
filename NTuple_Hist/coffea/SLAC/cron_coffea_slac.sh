e4f7f80478733738fb9c887d2d58cdc719e1c6fe
Juan <selbor903@gmail.com>
2025-02-07 15:11:20 -0800

    Changed cron script

--- NTuple_Hist/coffea/SLAC/cron_coffea_slac.sh
+++ NTuple_Hist/coffea/SLAC/cron_coffea_slac.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
 ssh iana "
-cd /sdf/data/atlas/u/selbor/AF-Benchmarking/NTuple_Hist/coffea/SLAC/
-
-srun run_example.sh
+srun /sdf/data/atlas/u/selbor/AF-Benchmarking/NTuple_Hist/coffea/SLAC/run_example.sh
 "
