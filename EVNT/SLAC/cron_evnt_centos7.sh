05c26decd9ccc935c36418e85ddaa4e82875339d
Juan <selbor903@gmail.com>
2025-02-07 13:08:22 -0800

    changed the path used in the scripts used by cron

--- EVNT/SLAC/cron_evnt_centos7.sh
+++ EVNT/SLAC/cron_evnt_centos7.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ssh iana "
-cd /sdf/scratch/users/s/selbor/EVNTJob/container_centos
+cd /sdf/data/atlas/u/selbor/EVNTJob/container_centos
 rm *
-sbatch /sdf/home/s/selbor/AF-Benchmarking/EVNT/SLAC/evnt_centos7_sub.sh"
+sbatch /sdf/data/atlas/u/selbor/AF-Benchmarking/EVNT/SLAC/evnt_centos7_sub.sh"
--- EVNT/SLAC/cron_evnt_el9.sh
+++ EVNT/SLAC/cron_evnt_el9.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ssh iana "
-cd /sdf/scratch/users/s/selbor/EVNTJob/container_el
+cd /sdf/data/atlas/u/selbor/EVNTJob/container_el
 rm *
-sbatch /sdf/home/s/selbor/AF-Benchmarking/EVNT/SLAC/evnt_el9_sub.sh"
+sbatch /sdf/data/atlas/u/selbor/AF-Benchmarking/EVNT/SLAC/evnt_el9_sub.sh"
