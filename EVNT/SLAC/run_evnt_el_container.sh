11a0f3005245bd2675729586264612c1e193fcd7
Juan <selbor903@gmail.com>
2025-02-10 10:36:52 -0800

    updated paths in the evnt scripts used at slac

--- EVNT/SLAC/run_evnt_el_container.sh
+++ EVNT/SLAC/run_evnt_el_container.sh
@@ -7,7 +7,7 @@ seed=1001
 config_dir="EVNTFiles/100xxx/100001/"
 
 # Copies input files dir to the working dir
-cp -r /sdf/data/atlas/u/$USER/EVNTFiles/ .
+cp -r /sdf/scratch/atlas/$USER/EVNTFiles/ .
 
 asetup AthGeneration,23.6.34,here
 Gen_tf.py --ecmEnergy=13000.0 --jobConfig=${config_dir}  --outputEVNTFile=EVNT.root --maxEvents=10000 --randomSeed=${seed}
@@ -15,7 +15,7 @@ Gen_tf.py --ecmEnergy=13000.0 --jobConfig=${config_dir}  --outputEVNTFile=EVNT.r
 # Current time used for log file storage
 curr_time=$(date +"%Y.%m.%dT%H")
 # Defines the output directory
-output_dir="/sdf/data/atlas/u/$USER/benchmarks/$curr_time/EVNT_container_el/"
+output_dir="/sdf/scratch/atlas/$USER/benchmarks/$curr_time/EVNT_container_el/"
 # Creates the output directory
 mkdir -p ${output_dir}
 # Obtains and appends the host name and payload size to the log file
