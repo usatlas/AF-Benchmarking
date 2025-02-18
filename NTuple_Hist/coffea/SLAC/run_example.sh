64050c8c5feeeb03ebea0b1154580ab906876361
Juan <selbor903@gmail.com>
2025-02-18 15:11:33 -0800

    Changed the paths used

--- NTuple_Hist/coffea/SLAC/run_example.sh
+++ NTuple_Hist/coffea/SLAC/run_example.sh
@@ -5,17 +5,17 @@
 curr_time=$(date +"%Y.%m.%dT%H")
 
 # Working directory
-work_dir="/sdf/scratch/atlas/selbor/Ntuple_Hist/coffea"
+work_dir="/sdf/home/s/selbor/Ntuple_Hist/coffea"
 
 cd ${work_dir}
 
-source ../bin/activate
+source /sdf/data/atlas/u/selbor/testing/bin/activate
 
 start_time=$date
 
 $start_time >> coffea_hist.log
 
-python3 ../../AF-Benchmarking/NTuple_Hist/coffea/SLAC/example.py >> coffea_hist.log
+python3 /sdf/data/atlas/u/selbor/AF-Benchmarking/NTuple_Hist/coffea/SLAC/example.py >> coffea_hist.log
 
 end_time=$date
 
@@ -27,7 +27,7 @@ du ntuple_cfw.pdf >> coffea_hist.log
 
 rm ntuple_cfw.pdf
 
-log_file_dir="/sdf/scratch/atlas/selbor/benchmarks/${curr_time}/Coffea_Hist/"
+log_file_dir="/sdf/home/s/selbor/benchmarks/${curr_time}/Coffea_Hist/"
 
 mkdir -p ${log_file_dir}
 
