4d90aeb44e91a250bcb9d1318ecd9752c1f94ddd
Juan <selbor903@gmail.com>
2025-02-19 10:03:32 -0800

    Updated the paths used in files and reworked variables used

--- TRUTH3/SLAC/cron_truth3_el9_batch.sh
+++ TRUTH3/SLAC/cron_truth3_el9_batch.sh
@@ -5,4 +5,4 @@ cd /sdf/home/s/selbor/TRUTH3Job/container_el
 
 rm -r *
 
-sbatch /sdf/data/atlas/u/selbor/AF-Benchmarking/TRUTH3/SLAC/truth3_el9_sub.sh"
+sbatch /sdf/home/s/selbor/AF-Benchmarking/TRUTH3/SLAC/truth3_el9_sub.sh"
--- TRUTH3/SLAC/run_truth3_el9_container.sh
+++ TRUTH3/SLAC/run_truth3_el9_container.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-cp -r /sdf/data/atlas/u/$USER/AF-Benchmarking/TRUTH3/EVNT.root .
+
+username=$USER
+first_letter=${username:0:1}
+
+cp -r /sdf/home/$first_letter/$USER/AF-Benchmarking/TRUTH3/EVNT.root .
 asetup Athena,24.0.53,here
 Derivation_tf.py --CA True --inputEVNTFile EVNT.root --outputDAODFile=TRUTH3.root --formats TRUTH3
 
@@ -8,8 +12,6 @@ Derivation_tf.py --CA True --inputEVNTFile EVNT.root --outputDAODFile=TRUTH3.roo
 curr_time=$(date +"%Y.%m.%dT%H")
 
 # Defines the output directory where the log file will be stored
-username=$USER
-first_letter=${username:0:1}
 output_dir="/sdf/home/$first_letter/$USER/benchmarks/${curr_time}/TRUTH3_el9_container"
 
 # Creates the output directory
