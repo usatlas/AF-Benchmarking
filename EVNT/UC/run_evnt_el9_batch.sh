f358b46e0fc8e7b4a74a5982d417c584940b56ad
Juan <selbor903@gmail.com>
2025-02-26 13:19:15 -0800

    piped the output to another file

--- EVNT/UC/run_evnt_el9_batch.sh
+++ EVNT/UC/run_evnt_el9_batch.sh
@@ -20,7 +20,7 @@ echo $(date +"%H:%M:%S") >> split.log
 ## -m : mounts a specific directory
 ## -r : precedes the commands we want to run within the container
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OS_container} -m /data:/data -r "asetup AthGeneration,23.6.34,here && \
-Gen_tf.py --ecmEnergy=13000.0 --jobConfig=${config_dir}  --outputEVNTFile=EVNT.root --maxEvents=10000 --randomSeed=${seed}" 2>&1 | tee pipe_file.log
+Gen_tf.py --ecmEnergy=13000.0 --jobConfig=${config_dir}  --outputEVNTFile=EVNT.root --maxEvents=10000 --randomSeed=${seed} 2>&1 | tee pipe_file.log"
  
 # Appends time after Gen_tf.py to a log file
 echo $(date +"%H:%M:%S") >> split.log
