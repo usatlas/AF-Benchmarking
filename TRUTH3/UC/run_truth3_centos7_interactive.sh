3700a578837867d8a479baea6ac1e92615d08fbd
Juan <selbor903@gmail.com>
2025-02-26 11:28:41 -0800

    Split the log file information

--- TRUTH3/UC/run_truth3_centos7_interactive.sh
+++ TRUTH3/UC/run_truth3_centos7_interactive.sh
@@ -6,12 +6,18 @@ config_dir="/data/$USER/TRUTH3_StaticDir/"
 # Sets up the ATLAS Environment
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 
+# Appends time before Reco_tf.py to log file
+echo $(date +"%H:%M:%S") >> split.log
+
 # Sets up the container:
 ## -c : used to make a container followed by the OS we want to use
 ## -m : mounts a specific directory
 ## -r : precedes the commands we want to run within the container
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c centos7 -m /data:/data -r "asetup AthDerivation,21.2.178.0,here && \
   Reco_tf.py --inputEVNTFile ${config_dir}EVNT_centos_interactive.root --outputDAODFile=TRUTH3.root --reductionConf TRUTH3"
+
+# Appends time after Reco_tf.py to a log file
+echo $(date +"%H:%M:%S") >> split.log
 
 # current time used for log file storage
 curr_time=$(date +"%Y.%m.%dT%H")
@@ -23,8 +29,9 @@ output_dir="/data/$USER/benchmarks/$curr_time/TRUTH3_centos_interactive/"
 mkdir -p ${output_dir}
 
 # Obtains and appends the host name and payload size to the log file
-hostname >> log.EVNTtoDAOD
-du DAOD_TRUTH3.TRUTH3.root >> log.EVNTtoDAOD
+hostname >> split.log
+du DAOD_TRUTH3.TRUTH3.root >> split.log
 
 # Moves the log file to the output directory
 mv log.EVNTtoDAOD ${output_dir}
+mv split.log ${output_dir}
--- TRUTH3/UC/run_truth3_el9_interactive.sh
+++ TRUTH3/UC/run_truth3_el9_interactive.sh
@@ -9,12 +9,18 @@ OScontainer="el9"
 # Sets up the working environment
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 
+# Appends time before Derivation_tf.py to log file
+echo $(date +"%H:%M:%S") >> split.log
+
 # Sets up the container:
 ## -c : used to make a container followed by the OS we want to use
 ## -m : mounts a specific directory
 ## -r : precedes the commands we want to run within the container
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OScontainer} -m /data:/data -r "asetup Athena,24.0.53,here && \
   Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT_el9_interactive.root --outputDAODFile=TRUTH3.root --formats TRUTH3"
+
+# Appends time after Derivation_tf.py to a log file
+echo $(date +"%H:%M:%S") >> split.log
 
 # Current time used for file storage
 curr_time=$(date +"%Y.%m.%dT%H")
@@ -26,8 +32,9 @@ output_dir="/data/$USER/benchmarks/$curr_time/TRUTH3_el9_container_interactive"
 mkdir -p ${output_dir}
 
 # Obtains and appends the host name and payload size to the log file
-hostname >> log.Derivation
-du DAOD_TRUTH3.TRUTH3.root >> log.Derivation
+hostname >> split.log
+du DAOD_TRUTH3.TRUTH3.root >> split.log
 
 # Moves the log file to the output directory
 mv log.Derivation ${output_dir}
+mv split.log ${output_dir}
