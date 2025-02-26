c8c2b82d3f077ce0e40e38645fe0ee9a69888bf3
Juan <selbor903@gmail.com>
2025-02-26 10:38:09 -0800

    Added lines that split information between log files

--- EVNT/UC/run_evnt_native_batch.sh
+++ EVNT/UC/run_evnt_native_batch.sh
@@ -10,9 +10,15 @@ config_dir="/data/$(whoami)/evnt/100xxx/100001"
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
 
+# Appends time before Gen_tf.py to log file
+echo $(date +"%H:%M:%S") >> split.log
+
 # Sets up the Ath* version
 asetup AthGeneration,23.6.34,here
 Gen_tf.py --ecmEnergy=13000.0 --jobConfig=${config_dir}  --outputEVNTFile=EVNT.root --maxEvents=10000 --randomSeed=${seed}
+
+# Appends time before Gen_tf.py to a log file
+echo $(date +"%H:%M:%S") >> split.log
 
 # Current time used for log file storage
 curr_time=$(date +"%Y.%m.%dT%H")
@@ -23,10 +29,11 @@ output_dir="/data/$(whoami)/benchmarks/${curr_time}/EVNT/"
 # Makes the output directory
 mkdir -p ${output_dir}
 
-# Appends the host name and the payload size to the log file
-hostname >> log.generate
-du EVNT.root >> log.generate
+# Appends the hostname and payload size to the log files
+hostname >> split.log
+du EVNT.root >> split.log
 
 # Moves the log file to the output directory
 mv log.generate ${output_dir}
+mv split.log ${output_dir}
 
