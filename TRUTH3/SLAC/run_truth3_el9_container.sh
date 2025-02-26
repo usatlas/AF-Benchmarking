80b40b51e37947ac2350f4a8413d7efd08608949
Juan <selbor903@gmail.com>
2025-02-26 11:39:40 -0800

    Split the log file information

--- TRUTH3/SLAC/run_truth3_el9_container.sh
+++ TRUTH3/SLAC/run_truth3_el9_container.sh
@@ -5,8 +5,15 @@ username=$USER
 first_letter=${username:0:1}
 
 cp -r /sdf/home/$first_letter/$USER/AF-Benchmarking/TRUTH3/EVNT.root .
+
+# Appends time before Derivation_tf.py to log file
+echo $(date +"%H:%M:%S") >> split.log
+
 asetup Athena,24.0.53,here
 Derivation_tf.py --CA True --inputEVNTFile EVNT.root --outputDAODFile=TRUTH3.root --formats TRUTH3
+
+# Appends time after Derivation_tf.py to a log file
+echo $(date +"%H:%M:%S") >> split.log
 
 # Defines the current time
 curr_time=$(date +"%Y.%m.%dT%H")
@@ -17,8 +24,9 @@ output_dir="/sdf/home/$first_letter/$USER/benchmarks/${curr_time}/TRUTH3_el9_con
 # Creates the output directory
 mkdir -p ${output_dir}
 # Appends the host-name to the end of the log file
-hostname >> log.Derivation
+hostname >> split.log
 # Appends the size of the output DAOD file to the end of the log file
-du DAOD_TRUTH3.TRUTH3.root >> log.Derivation
+du DAOD_TRUTH3.TRUTH3.root >> split.log
 # Moves the log file to the output directory defined above
 mv log.Derivation ${output_dir}
+mv split.log ${output_dir}
