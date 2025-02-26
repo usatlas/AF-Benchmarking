55e078c4eb76a107487e59d494e319b08ceb8332
Juan <selbor903@gmail.com>
2025-02-26 11:39:40 -0800

    Split the log file information

--- TRUTH3/SLAC/run_truth3_centos7_container.sh
+++ TRUTH3/SLAC/run_truth3_centos7_container.sh
@@ -6,9 +6,15 @@ first_letter=${username:0:1}
 
 cp -r /sdf/home/$first_letter/$USER/AF-Benchmarking/TRUTH3/EVNT.root .
 
+# Appends time before Reco_tf.py to log file
+echo $(date +"%H:%M:%S") >> split.log
+
 
 asetup AthDerivation,21.2.178.0,here
 Reco_tf.py --inputEVNTFile EVNT.root --outputDAODFile=TRUTH3.root --reductionConf TRUTH3
+
+# Appends time after Reco_tf.py to a log file
+echo $(date +"%H:%M:%S") >> split.log
 
 # Defines the current time
 curr_time=$(date +"%Y.%m.%dT%H")
@@ -17,8 +23,9 @@ output_dir="/sdf/home/$first_letter/$USER/benchmarks/${curr_time}/TRUTH3_centos7
 # Creates the output directory
 mkdir -p ${output_dir}
 # Appends the host-name to the end of the log file
-hostname >> log.EVNTtoDAOD
+hostname >> split.log
 # Appends the size of the output DAOD file to the end of the log file
-du DAOD_TRUTH3.TRUTH3.root >> log.EVNTtoDAOD
+du DAOD_TRUTH3.TRUTH3.root >> split.log
 # Moves the log file to the output directory defined above
 mv log.EVNTtoDAOD ${output_dir}
+mv split.log ${output_dir}
