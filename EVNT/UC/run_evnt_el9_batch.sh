a1698d71aa3b725561d90060e7af17b8b39574d0
Juan <selbor903@gmail.com>
2025-02-26 10:36:44 -0800

    Added lines that send information used when parsing to a different file to make up for the cases where the log file only has two lines

--- EVNT/UC/run_evnt_el9_batch.sh
+++ EVNT/UC/run_evnt_el9_batch.sh
@@ -12,13 +12,19 @@ config_dir="/data/$(whoami)/evnt_el9/100xxx/100001"
 # Setting up the working environment
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 
+# Appends time before Gen_tf.py to log file
+echo $(date +"%H:%M:%S") >> split.log
+
 # Sets up the container:
 ## -c : used to make a container followed by the OS we want to use
 ## -m : mounts a specific directory
 ## -r : precedes the commands we want to run within the container
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OS_container} -m /data:/data -r "asetup AthGeneration,23.6.34,here && \
 Gen_tf.py --ecmEnergy=13000.0 --jobConfig=${config_dir}  --outputEVNTFile=EVNT.root --maxEvents=10000 --randomSeed=${seed}"
-  
+ 
+# Appends time before Gen_tf.py to a log file
+echo $(date +"%H:%M:%S") >> split.log
+
 # Current time used for log file storage
 curr_time=$(date +"%Y.%m.%dT%H")
 
@@ -27,8 +33,9 @@ output_dir="/data/$(whoami)/benchmarks/$curr_time/EVNT_contained_el9/"
 mkdir -p ${output_dir}
 
 # Obtains and appends the host name and payload size to the log file
-hostname >> log.generate
-du EVNT.root >> log.generate
+hostname >> split.log
+du EVNT.root >> split.log
 
-# Moves the log file to the output directory
+# Moves the log file and date_name file to the output directory
 mv log.generate ${output_dir}
+mv split.log ${output_dir}
