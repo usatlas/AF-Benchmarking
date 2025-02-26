59db01a9fe3389f79392646eccfb3a4a0bb83456
Juan <selbor903@gmail.com>
2025-02-26 11:28:41 -0800

    Split the log file information

--- TRUTH3/UC/run_truth3_native_interactive.sh
+++ TRUTH3/UC/run_truth3_native_interactive.sh
@@ -7,9 +7,15 @@ config_dir="/data/$USER/TRUTH3_StaticDir/"
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
 
+# Appends time before Derivation_tf.py to log file
+echo $(date +"%H:%M:%S") >> split.log
+
 # Sets the Athena version we want
 asetup Athena,24.0.53,here
 Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT_interactive.root --outputDAODFile=TRUTH3.root --formats TRUTH3
+
+# Appends time after Derivation_tf.py to a log file
+echo $(date +"%H:%M:%S") >> split.log
 
 # current time used for log file storage
 curr_time=$(date +"%Y.%m.%dT%H")
@@ -21,8 +27,9 @@ output_dir="/data/$USER/benchmarks/$curr_time/TRUTH3_interactive"
 mkdir -p ${output_dir}
 
 # Obtains and appends the host machine and payload size to the log file
-hostname >> log.Derivation
-du DAOD_TRUTH3.TRUTH3.root >> log.Derivation
+hostname >> split.log
+du DAOD_TRUTH3.TRUTH3.root >> split.log
 
 # Moves the log file to the output directory
 mv log.Derivation ${output_dir}
+mv split.log ${output_dir}
