9d1ba34fba94b4dbbb4bc3211828658d414a8d8b
Juan <selbor903@gmail.com>
2025-03-04 20:58:40 -0800

    Adding variable with min and hour

--- batch_benchmarks/condor_status.sh
+++ batch_benchmarks/condor_status.sh
@@ -4,8 +4,11 @@
 # Time of running this script
 curr_date_hour=$(date +"%Y.%m.%dT%H")
 
+# Gets the date.Hour.minute
+curr_hour_min=$(date +"%Y.%m.%dT%H.%M")
+
 # Getting information from condor
-condor_status -compact 2>&1 | tee condor_log_${curr_date_hour}.log
+condor_status -compact 2>&1 | tee condor_log_${curr_hour_min}.log
 
 # Moves the log file to the benchmarks directory
 mv condor_log_${curr_date_hour}.log /data/selbor/benchmarks/${curr_date_hour}/condor_info
