0690c3baf8c4891b91fa95ca1bfc005dc2308c03
Juan <selbor903@gmail.com>
2025-03-12 09:35:55 -0700

    pushing different method used at bnl

--- EVNT/BNL/run_evnt_el9_batch.sh
+++ EVNT/BNL/run_evnt_el9_batch.sh
@@ -19,7 +19,7 @@ source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OScontainer} -r "as
 curr_time=$(date +"%Y.%m.%dT%H")
 
 # Output directory
-output_dir="/atlasgpfs01/usatlas/data/$USER/benchmarks/$curr_time/EVNT_el9_batch"
+output_dir="/usatlas/u/$USER/benchmarks/$curr_time/EVNT_el9_batch"
 
 # Creates the output directory
 mkdir -p ${output_dir}
