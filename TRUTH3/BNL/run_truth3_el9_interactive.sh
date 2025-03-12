43c8ae3bc1dc54cc32f0d24e9d9d3f2e5ba7d05b
Juan <selbor903@gmail.com>
2025-03-12 14:58:01 -0700

    Made the interactive version of the scripts

--- TRUTH3/BNL/run_truth3_el9_interactive.sh
+++ TRUTH3/BNL/run_truth3_el9_interactive.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Config Dir Needed
-config_dir="TRUTH3Files/"
+# Copying input files to working directory
+cp -r ~/AF-Benchmarking/TRUTH3/EVNT.root .
 
 # Sets up the environment
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
@@ -10,21 +10,31 @@ export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 ## -c : used to make a container followed by the OS we want to use
 ## -m : mounts a specific directory
 ## -r : precedes the commands we want to run within the container
-source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -r "asetup Athena,24.0.53,here && \
-  Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT_el9_interactive.root --outputDAODFile=TRUTH3.root --formats TRUTH3"
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -r "asetup Athena,24.0.53,here &&\
+  echo $(date +"%Y.%m.%d.%H.%S") >> split.log &&\
+  Derivation_tf.py --CA True --inputEVNTFile EVNT.root --outputDAODFile=TRUTH3.root --formats TRUTH3 2>&1 | tee pipe_file.log &&\
+  echo $(date +"%Y.%m.%d.%H.%S") >> split.log"
 
 # Current time used for file storage
 curr_time=$(date +"%Y.%m.%dT%H")
 
 # Defines the output directory
-output_dir="/atlasgpfs01/usatlas/data/jroblesgo/benchmarks/$curr_time/TRUTH3_el9_interactive"
+output_dir="/usatlas/u/jroblesgo/benchmarks/${curr_time}/TRUTH3_el9_interactive"
 
 # Creates the output directory
 mkdir -p ${output_dir}
 
 # Obtains and appends the host name and payload size to the log file
-hostname >> log.Derivation
-du DAOD_TRUTH3.TRUTH3.root >> log.Derivation
+hostname >> split.log
+du DAOD_TRUTH3.TRUTH3.root >> split.log
 
 # Moves the log file to the output directory
 mv log.Derivation ${output_dir}
+mv split.log ${output_dir}
+mv pipe_file.log ${output_dir}
+
+# Checks the directory, if it matches it cleans it for the next job
+if [ $(pwd)="/usatlas/u/jroblesgo/TRUTH3Job/el_i" ]; then
+  rm -r *
+fi
+
---
+++ TRUTH3/BNL/run_truth3_native_interactive.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Copying input files to working directory
+cp -r ~/AF-Benchmarking/TRUTH3/EVNT.root .
+
+# Sets up our environment
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
+
+# Sets the Athena version we want
+asetup Athena,24.0.53,here
+
+# Appends time before Derivation_tf.py to log file
+echo $(date +"%H:%M:%S") >> split.log
+
+
+Derivation_tf.py --CA True --inputEVNTFile EVNT.root --outputDAODFile=TRUTH3.root --formats TRUTH3 2>&1 | tee pipe_file.log
+
+# Appends time after Derivation_tf.py to a log file
+echo $(date +"%H:%M:%S") >> split.log
+
+# current time used for log file storage
+curr_time=$(date +"%Y.%m.%dT%H")
+
+# Defines the output directory
+output_dir="/usatlas/u/jroblesgo/benchmarks/${curr_time}/TRUTH3_native_interactive"
+
+# Creates the output directory
+mkdir -p ${output_dir}
+
+# Obtains and appends the host machine and payload size to the log file
+hostname >> split.log
+du DAOD_TRUTH3.TRUTH3.root >> split.log
+
+# Moves the log file to the output directory
+mv log.Derivation ${output_dir}
+mv split.log ${output_dir}
+mv pipe_file.log ${output_dir}
+
+# Checks the directory, if it matches it cleans it for the next job
+if [ $(pwd)="/usatlas/u/jroblesgo/TRUTH3Job/native_i" ]; then
+  rm -r *
+fi
+
