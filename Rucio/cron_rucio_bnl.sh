fd058ac87c9f3a6152d61ffc567ffa80b2a3d81b
Juan <selbor903@gmail.com>
2025-03-12 15:31:13 -0700

    updated the cron file used at bnl

--- Rucio/cron_rucio_bnl.sh
+++ Rucio/cron_rucio_bnl.sh
@@ -1,13 +1,9 @@
-#! /bin/bash
+#!/bin/bash
 
-# Directory where I want the job to run from
-job_dir="/atlasgpfs01/usatlas/scratch/jroblesgo/RucioJob"
+# Job directory
+job_dir="/usatlas/u/jroblesgo/RucioJob"
 
-# Changes to desired directory
-cd ${job_dir}
-
-# IF the download exits it will remove it
-rm -r mc23_13p6TeV.700866.Sh_2214_WWW_3l3v_EW6.deriv.DAOD_PHYSLITE.e8532_e8528_s4162_s4114_r14622_r14663_p6491/
-
-# Calls the script that will trigger the job
-~/AF-Benchmarking/Rucio/rucio_script.sh
+if [ -d ${job_dir} ]; then
+  cd ${job_dir}
+  /usatlas/u/jroblesgo/AF-Benchmarking/Rucio/rucio_script.sh
+fi
