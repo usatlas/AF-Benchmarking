c12b0a29645b3dfb356508a89dcb3cb64a89aee9
Juan <selbor903@gmail.com>
2025-03-20 15:56:51 -0700

    Adding the event loop script that will run at BNL

---
+++ event_loop/BNL/run_photon_eventloop.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Time that will be used to store the log file
+curr_time=$(date +"%Y.%m.%dT%H")
+
+# Setting up
+setup ATLAS
+
+# Setting up root
+export ALRB_rootVersion=6.34.04-x86_64-el9-gcc13-opt
+lsetup root
+
+# Getting start date
+date >> photon_eventloop.log
+
+# Running the script
+python3 photon_ABCD_eventloop.py 2>&1 | tee photon_eventloop.log 
+
+# Getting end date
+date >> photon_eventloop.log
+
+# Getting host name
+hostname >> photon_eventloop.log
+
+mv photon_eventloop.log /data/$USER/benchmarks/$curr_time/photon_eventloop/
