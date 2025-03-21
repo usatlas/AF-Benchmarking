3a6036532c2020afd0de3308346fe05725999395
Juan <selbor903@gmail.com>
2025-03-21 15:31:06 -0700

    Mounting the directory with input files into my container

--- event_loop/SLAC/run_photon_eventloop.sh
+++ event_loop/SLAC/run_photon_eventloop.sh
@@ -6,7 +6,7 @@ curr_time=$(date +"%Y.%m.%dT%H")
 
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 export ALRB_localConfigDir=$HOME/localConfig
-source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -r "export ALRB_rootVersion=6.34.04-x86_64-el9-gcc13-opt && \
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -m /sdf/data/usatlas/u/selbor/ -r "export ALRB_rootVersion=6.34.04-x86_64-el9-gcc13-opt && \
   lsetup root && \
   date >> photon_eventloop.log && \
   python3 ~/AF-Benchmarking/event_loop/SLAC/photon_ABCD_eventloop.py 2>&1 | tee photon_eventloop.log  && \
