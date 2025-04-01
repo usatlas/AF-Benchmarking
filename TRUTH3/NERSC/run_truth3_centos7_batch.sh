4bbcc0e853444114e133b6fc6baa3f6a59dd3cf9
Juan <selbor903@gmail.com>
2025-04-01 14:43:55 -0700

    Changed the job script to split the log file

--- TRUTH3/NERSC/run_truth3_centos7_batch.sh
+++ TRUTH3/NERSC/run_truth3_centos7_batch.sh
@@ -13,12 +13,18 @@ cp ~/AF-Benchmarking/TRUTH3/EVNT.root .
 # Sets up the working environment
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 
+# Appends time before Reco_tf.py to log file
+echo $(date +"%H:%M:%S") >> split.log
+
 # Sets up the container:
 ## -c : used to make a container followed by the OS we want to use
 ## -m : mounts a specific directory
 ## -r : precedes the commands we want to run within the container
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c ${OScontainer} -r "asetup AthDerivation,21.2.178.0,here && \
-  Reco_tf.py --inputEVNTFile EVNT.root --outputDAODFile=TRUTH3.root --reductionConf TRUTH3"
+  Reco_tf.py --inputEVNTFile EVNT.root --outputDAODFile=TRUTH3.root --reductionConf TRUTH3 2>&1 | tee pipe_file.log"
+
+# Appends time after Reco_tf.py to a log file
+echo $(date +"%H:%M:%S") >> split.log
 
 rm EVNT.root
 
@@ -29,10 +35,11 @@ output_dir="$HOME/benchmarks/$curr_time/TRUTH3_centos/"
 mkdir -p ${output_dir}
 
 # Obtains and appends the host name and payload size to the log file
-hostname >> log.EVNTtoDAOD
-du DAOD_TRUTH3.TRUTH3.root >> log.EVNTtoDAOD
+hostname >> split.log
+du DAOD_TRUTH3.TRUTH3.root >> split.log
 
 # Moves the log file to the output directory
 mv log.EVNTtoDAOD ${output_dir}
-
+mv split.log ${output_dir}
+mv pipe_file.log ${output_dir}
 rm *
