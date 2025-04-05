34c4b84c461dbf605eb466701f32ada614f66a86
Juan <selbor903@gmail.com>
2025-04-05 15:49:59 -0700

    Updated the paths

--- parsing/NERSC/cron_parsing.sh
+++ parsing/NERSC/cron_parsing.sh
@@ -5,11 +5,13 @@ cd /global/homes/s/selbor/parsing_jobs
 
 source bin/activate
 
+python3 truth3_el/truth3_el_parsing.py
+python3 truth3_el_int/truth3_el_int_parsing.py
+python3 truth3_centos/truth3_centos_parsing.py
+python3 truth3_centos_int/ruth3_centos_int_parsing.py
+python3 evnt_el/evnt_el_parsing.py
+python3 evnt_centos/evnt_centos_parsing.py
+python3 rucio_download/rucio_parsing.py
 python3 coffea_parsing.py
-python3 truth3_el_parsing.py
-python3 truth3_centos_parsing.py
-python3 evnt_el_parsing.py
-python3 evnt_centos_parsing.py
-python3 rucio_parsing.py
 
 deactivate
