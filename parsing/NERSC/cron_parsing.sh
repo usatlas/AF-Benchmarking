a0a833fd5851e1cf581ee9f00f7b5ef25baf6241
Juan <selbor903@gmail.com>
2025-04-08 13:54:35 -0700

    Fixed typo

--- parsing/NERSC/cron_parsing.sh
+++ parsing/NERSC/cron_parsing.sh
@@ -8,7 +8,7 @@ source bin/activate
 python3 truth3_el/truth3_el_parsing.py
 python3 truth3_el_int/truth3_el_int_parsing.py
 python3 truth3_centos/truth3_centos_parsing.py
-python3 truth3_centos_int/ruth3_centos_int_parsing.py
+python3 truth3_centos_int/truth3_centos_int_parsing.py
 python3 evnt_el/evnt_el_parsing.py
 python3 evnt_centos/evnt_centos_parsing.py
 python3 rucio_download/rucio_parsing.py
