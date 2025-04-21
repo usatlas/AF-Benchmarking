2f80e752d1f770c4b6fd7a12e732a1d297edd7f2
Juan <selbor903@gmail.com>
2025-04-21 16:22:49 -0700

    updated scripts used at SLAC for the coffea job; running in a container now

--- NTuple_Hist/coffea/SLAC/run_example.sh
+++ NTuple_Hist/coffea/SLAC/run_example.sh
@@ -1,31 +1,8 @@
-#! /bin/bash
+#!/bin/bash
 
+# Setting up environment and container
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 
-# Gets the current time
-curr_time=$(date +"%Y.%m.%dT%H")
+export ALRB_localConfigDir=$HOME/localConfig
 
-# Working directory
-work_dir="/sdf/home/s/selbor/Ntuple_Hist/coffea"
-
-cd ${work_dir}
-
-source /sdf/data/atlas/u/selbor/testing/bin/activate
-
-start_time=$date
-
-$start_time >> split.log
-
-python3 /sdf/home/s/selbor/AF-Benchmarking/NTuple_Hist/coffea/SLAC/example.py 2>&1 | tee coffea_hist.log
-
-end_time=$date
-
-$end_time >> split.log
-
-hostname >> split.log
-
-log_file_dir="/sdf/home/s/selbor/benchmarks/${curr_time}/Coffea_Hist/"
-
-mkdir -p ${log_file_dir}
-
-mv coffea_hist.log ${log_file_dir}
-mv split.log ${log_file_dir}
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -m /sdf/data/atlas/ -r "python3 example.py"
