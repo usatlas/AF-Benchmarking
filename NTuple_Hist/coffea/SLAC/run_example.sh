2913e4e6e1a2b100806e29d200fb7ec23851dac7
Juan <selbor903@gmail.com>
2025-04-29 10:01:00 -0700

    Getting the date appended to a log file

--- NTuple_Hist/coffea/SLAC/run_example.sh
+++ NTuple_Hist/coffea/SLAC/run_example.sh
@@ -8,9 +8,7 @@ work_dir="/sdf/scratch/users/s/selbor/coffea_testing"
 
 cd ${work_dir}
 
-start_time=$date
-
-$start_time >> split.log
+date >> split.log
 
 # Setting up environment and container
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
@@ -19,9 +17,8 @@ export ALRB_localConfigDir=$HOME/localConfig
 
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -m /sdf/data/atlas/ -r "python3 example.py 2>&1 | tee coffea_hist.log"
 
-end_time=$date
 
-$end_time >> split.log
+date >> split.log
 
 hostname >> split.log
 
