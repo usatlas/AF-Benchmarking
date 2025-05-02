4d9b95a6c3addc5907c3948151222b2588534442
Juan <selbor903@gmail.com>
2025-05-02 13:50:35 -0700

    Running in a container

--- event_loop/BNL/run_photon_eventloop.sh
+++ event_loop/BNL/run_photon_eventloop.sh
@@ -5,18 +5,12 @@ curr_time=$(date +"%Y.%m.%dT%H")
 
 cd /atlasgpfs01/usatlas/scratch/jroblesgo/ntuple/event_loop/
 
-# Setting up
-setup ATLAS
-
-# Setting up root
-export ALRB_rootVersion=6.34.04-x86_64-el9-gcc13-opt
-lsetup root
-
-# Getting start date
-date >> photon_eventloop.log
-
-# Running the script
-python3 ~/AF-Benchmarking/event_loop/BNL/photon_ABCD_eventloop.py 2>&1 | tee photon_eventloop.log 
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+export ALRB_localConfigDir=$HOME/localConfig
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -m /atlasgpfs01/usatlas/data/ -r "export ALRB_rootVersion=6.34.04-x86_64-el9-gcc13-opt
+  lsetup root
+  date >> photon_eventloop.log
+  python3 ~/AF-Benchmarking/event_loop/BNL/photon_ABCD_eventloop.py 2>&1 | tee photon_eventloop.log"
 
 # Getting end date
 date >> photon_eventloop.log
