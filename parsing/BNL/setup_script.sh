96b29e0d8793a96465172c833f62a41a122441d0
Juan <selbor903@gmail.com>
2025-05-12 12:58:04 -0700

    Container script used for parsing jobs

---
+++ parsing/BNL/setup_script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+export ALRB_localConfigDir=$HOME/localConfig
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -m /usatlas/u/jroblesgo/ -r "lsetup 'python 3.9.22-x86_64-el9'
+python3 rucio_parsing.py
+"
