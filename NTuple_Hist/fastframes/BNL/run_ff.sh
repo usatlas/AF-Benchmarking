aa27b54069c155535fb180574c3e71c9eda61048
Juan <selbor903@gmail.com>
2025-05-14 09:43:40 -0700

    Updated script that runs ff job at BNL

--- NTuple_Hist/fastframes/BNL/run_ff.sh
+++ NTuple_Hist/fastframes/BNL/run_ff.sh
@@ -2,42 +2,22 @@
 
 curr_date=$(date +"%Y.%m.%dT%H")
 
+cd /atlasgpfs01/usatlas/data/jroblesgo/FFJob
+
 # Sets up ATLAS environment
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 export ALRB_localConfigDir=$HOME/localConfig
-source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
-
-# Sets up root and boost
-asetup StatAnalysis,0.5.0
-
-# Sets up emi
-lsetup emi
-
-# Gives certificate credentials
-cat $HOME/pass.txt | voms-proxy-init -voms atlas
-
-lsetup "rucio -w"
-
-# Go into the class's build directory
-cd /atlasgpfs01/usatlas/data/jroblesgo/TutorialClass/build
-
-# Setting paths
-source setup.sh
-
-# Return to the job's directory
-cd -
-
-# Getting the date and time before running script
-echo date >> ff.log
-
-# Running the FastFrames script
-python3 /atlasgpfs01/usatlas/data/jroblesgo/FastFrames/python/FastFrames.py -c $HOME/AF-Benchmarking/NTuple_Hist/fastframes/BNL/example_config.yml 2>&1 | tee ff.log
-
-# Getting the date and time after running script
-echo date >> ff.log
-
-# Getting the host-machine's name
-echo hostname >> ff.log
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -m /atlasgpfs01/usatlas/data/ -r "asetup StatAnalysis,0.5.0 &&\
+  lsetup emi &&\
+  cat $HOME/pass.txt | voms-proxy-init -voms atlas &&\
+  lsetup "rucio -w" &&\
+  cd /atlasgpfs01/usatlas/data/jroblesgo/TutorialClass/build &&\
+  source setup.sh &&\
+  cd - &&\
+  echo date >> ff.log &&\
+  python3 /atlasgpfs01/usatlas/data/jroblesgo/FastFrames/python/FastFrames.py -c $HOME/AF-Benchmarking/NTuple_Hist/fastframes/BNL/example_config.yml 2>&1 | tee ff.log &&\
+  echo date >> ff.log &&\
+  echo hostname >> ff.log"
 
 # output directory
 output_dir="/atlasgpfs01/usatlas/data/jroblesgo/benchmarks/${curr_date}/FF_NTuple"
