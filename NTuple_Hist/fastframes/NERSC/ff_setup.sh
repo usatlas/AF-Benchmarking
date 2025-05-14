1c59943fd4ca9ba404b34d797b6496803ddd180f
Juan <selbor903@gmail.com>
2025-05-14 10:34:38 -0700

    removed old file

--- NTuple_Hist/fastframes/NERSC/ff_setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
-export ALRB_localConfigDir=$HOME/localConfig
-source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -r"asetup StatAnalysis,0.5.0 &&\
-  
