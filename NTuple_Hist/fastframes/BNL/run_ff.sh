07c318c4e027311ea2e8d78e45ee726493e62295
Juan <selbor903@gmail.com>
2025-05-14 09:45:39 -0700

    Updated script that runs ff job at BNL

--- NTuple_Hist/fastframes/BNL/run_ff.sh
+++ NTuple_Hist/fastframes/BNL/run_ff.sh
@@ -7,7 +7,7 @@ cd /atlasgpfs01/usatlas/data/jroblesgo/FFJob
 # Sets up ATLAS environment
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 export ALRB_localConfigDir=$HOME/localConfig
-source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -m /atlasgpfs01/usatlas/data/ -r "asetup StatAnalysis,0.5.0 &&\
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -m /atlasgpfs01/usatlas/data/jroblesgo/ -r "asetup StatAnalysis,0.5.0 &&\
   lsetup emi &&\
   cat $HOME/pass.txt | voms-proxy-init -voms atlas &&\
   lsetup "rucio -w" &&\
