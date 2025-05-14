06c9cf7d57ddb7a658502181b31004e822837589
Juan <selbor903@gmail.com>
2025-05-14 13:23:54 -0700

    Updated script that runs ff job at NERSC

--- NTuple_Hist/fastframes/NERSC/run_ff.sh
+++ NTuple_Hist/fastframes/NERSC/run_ff.sh
@@ -6,10 +6,10 @@ curr_date=$(date +"%Y.%m.%dT%H")
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 export ALRB_localConfigDir=$HOME/localConfig
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -r "asetup StatAnalysis,0.5.0
-  cd /srv/TutorialClass/build
-  source setup.sh
-  cd -
-  echo date >> ff.log
+  cd /srv/TutorialClass/build &&\
+  source setup.sh &&\
+  cd - &&\
+  echo date >> ff.log &&\
   python3 /srv/FastFrames/python/FastFrames.py -c $HOME/AF-Benchmarking/NTuple_Hist/fastframes/NERSC/example_config.yml 2>&1 | tee ff.log"
   echo date >> ff.log
   echo hostname >> ff.log
