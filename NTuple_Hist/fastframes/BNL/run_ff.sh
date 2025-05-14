6fb87988a01bd4d6a083a72e24674c5cb97b20a4
Juan <selbor903@gmail.com>
2025-05-14 16:05:37 -0700

    updated the script used to run the ff job at BNL

--- NTuple_Hist/fastframes/BNL/run_ff.sh
+++ NTuple_Hist/fastframes/BNL/run_ff.sh
@@ -2,22 +2,21 @@
 
 curr_date=$(date +"%Y.%m.%dT%H")
 
+
 # Sets up ATLAS environment
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 export ALRB_localConfigDir=$HOME/localConfig
-source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -r "asetup StatAnalysis,0.5.0
-  lsetup emi
-  cat $HOME/pass.txt | voms-proxy-init -voms atlas
-  cd /srv/TutorialClass/build
-  source setup.sh
-  cd -
-  echo date >> ff.log
-  python3 /srv/FastFrames/python/FastFrames.py -c $HOME/AF-Benchmarking/NTuple_Hist/fastframes/BNL/example_config.yml 2>&1 | tee ff.log"
-  echo date >> ff.log
-  echo hostname >> ff.log
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -r "asetup StatAnalysis,0.5.0 &&\
+  cmake3 -S /srv/TutorialClass/FastFrames -B /srv/TutorialClass/build &&\
+  cmake3 --build /srv/TutorialClass/build -j 16 &&\
+  source /srv/TutorialClass/buildsetup.sh &&\
+  echo date >> ff.log &&\
+  python3 /srv/FastFrames/python/FastFrames.py -c /srv/input/example_config.yml 2>&1 | tee ff.log"
+echo date >> ff.log
+echo hostname >> ff.log
 
 # output directory
-output_dir="/atlasgpfs01/usatlas/data/jroblesgo/benchmarks/${curr_date}/FF_NTuple"
+output_dir="/atlasgpfs01/usatlas/data/jroblesgo/benchmarks/$curr_date/FF_NTuple"
 
 # Creates output dir
 mkdir -p ${output_dir}
