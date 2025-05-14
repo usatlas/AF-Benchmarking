f6b8506f3df1f2b5e02dfe125c4b0641a89fbfd3
Juan <selbor903@gmail.com>
2025-05-14 13:16:59 -0700

    Updated script that runs ff job at NERSC

--- NTuple_Hist/fastframes/NERSC/run_ff.sh
+++ NTuple_Hist/fastframes/NERSC/run_ff.sh
@@ -8,8 +8,6 @@ ddir
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 export ALRB_localConfigDir=$HOME/localConfig
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -r "asetup StatAnalysis,0.5.0
-  lsetup emi
-  cat $HOME/pass.txt | voms-proxy-init -voms atlas
   cd /srv/TutorialClass/build
   source setup.sh
   cd -
