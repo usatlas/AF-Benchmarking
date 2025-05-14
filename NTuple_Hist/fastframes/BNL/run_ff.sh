c25337ef0715c7a69ebcf7c9df28c6116cceb338
Juan <selbor903@gmail.com>
2025-05-14 09:57:01 -0700

    Updated script that runs ff job at BNL

--- NTuple_Hist/fastframes/BNL/run_ff.sh
+++ NTuple_Hist/fastframes/BNL/run_ff.sh
@@ -6,14 +6,14 @@ curr_date=$(date +"%Y.%m.%dT%H")
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 export ALRB_localConfigDir=$HOME/localConfig
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9  -r "asetup StatAnalysis,0.5.0
-  lsetup emi
-  cat $HOME/pass.txt | voms-proxy-init -voms atlas
-  cd /atlasgpfs01/usatlas/data/jroblesgo/TutorialClass/build
-  source setup.sh
-  cd -
-  echo date >> ff.log
-  python3 /atlasgpfs01/usatlas/data/jroblesgo/FastFrames/python/FastFrames.py -c $HOME/AF-Benchmarking/NTuple_Hist/fastframes/BNL/example_config.yml 2>&1 | tee ff.log
-  echo date >> ff.log
+  lsetup emi &&\
+  cat $HOME/pass.txt | voms-proxy-init -voms atlas &&\
+  cd /atlasgpfs01/usatlas/data/jroblesgo/TutorialClass/build &&\
+  source setup.sh &&\
+  cd - &&\
+  echo date >> ff.log &&\
+  python3 /atlasgpfs01/usatlas/data/jroblesgo/FastFrames/python/FastFrames.py -c $HOME/AF-Benchmarking/NTuple_Hist/fastframes/BNL/example_config.yml 2>&1 | tee ff.log &&\
+  echo date >> ff.log &&\
   echo hostname >> ff.log"
 
 # output directory
