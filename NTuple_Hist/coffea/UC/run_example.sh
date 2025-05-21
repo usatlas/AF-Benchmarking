f136958029f79b8dc8962ed9fa9ceb7d9306dda0
selbor <jprobles@ucsc.edu>
2025-05-21 14:51:09 -0700

    testing

--- NTuple_Hist/coffea/UC/run_example.sh
+++ NTuple_Hist/coffea/UC/run_example.sh
@@ -22,7 +22,7 @@ cp /home/$(whoami)/AF-Benchmarking/NTuple_Hist/coffea/light_roast-0.1.dev10+ge21
 # Setting up environment and container
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 export ALRB_localConfigDir=$HOME/localConfig
-source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -m /data/ -r " python3 --version >> /home/selbor/python3_version.txt"
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -m /data/ -r " python3 --version >> python3_version.txt"
 #source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -m /data/ -r "source /data/selbor/ntuple_hist/bin/activate
 #python3 example.py  2>&1 | tee coffea_hist.log"
 
