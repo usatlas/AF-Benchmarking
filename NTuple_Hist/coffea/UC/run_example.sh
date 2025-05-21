f58d43c12c67b7223f6d8d37b14ceb27d04ae527
selbor <jprobles@ucsc.edu>
2025-05-21 12:23:32 -0700

    Updating script to include install instructions to avoid the missing dask error

--- NTuple_Hist/coffea/UC/run_example.sh
+++ NTuple_Hist/coffea/UC/run_example.sh
@@ -21,7 +21,8 @@ cp /home/$(whoami)/AF-Benchmarking/NTuple_Hist/coffea/UC/example.py .
 # Setting up environment and container
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 export ALRB_localConfigDir=$HOME/localConfig
-source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -m /data/ -r "python3 example.py  2>&1 | tee coffea_hist.log"
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -m /data/ -r "pip3 install ~/AF-Benchmarking/NTuple_Hist/coffea/light_roast-0.1.dev10+ge21defc-py3-none-any.whl
+python3 example.py  2>&1 | tee coffea_hist.log"
 
 date >> split.log
 
