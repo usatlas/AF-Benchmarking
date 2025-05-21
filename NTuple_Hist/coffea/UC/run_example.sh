40072293effe34dd0e27a84baed32d6ca31935c4
Juan <selbor903@gmail.com>
2025-05-20 17:04:21 -0700

    Updated the path used to submit the job

--- NTuple_Hist/coffea/UC/run_example.sh
+++ NTuple_Hist/coffea/UC/run_example.sh
@@ -16,10 +16,12 @@ fi
 
 date >> split.log
 
+cp /home/$(whoami)/AF-Benchmarking/NTuple_Hist/coffea/UC/example.py .
+
 # Setting up environment and container
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 export ALRB_localConfigDir=$HOME/localConfig
-source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -m /data/ -r "python3 /home/$(whoami)/AF-Benchmarking/NTuple_Hist/coffea/UC/example.py  2>&1 | tee coffea_hist.log"
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -m /data/ -r "python3 example.py  2>&1 | tee coffea_hist.log"
 
 date >> split.log
 
@@ -31,3 +33,4 @@ mkdir -p ${log_file_dir}
 
 mv coffea_hist.log ${log_file_dir}
 mv split.log ${log_file_dir}
+
