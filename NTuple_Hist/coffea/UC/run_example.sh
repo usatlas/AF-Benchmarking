2109ae2ed43e16aaf86af4c7743ee5bec402f928
selbor <jprobles@ucsc.edu>
2025-05-21 15:26:13 -0700

    testing

--- NTuple_Hist/coffea/UC/run_example.sh
+++ NTuple_Hist/coffea/UC/run_example.sh
@@ -22,7 +22,8 @@ cp /home/$(whoami)/AF-Benchmarking/NTuple_Hist/coffea/light_roast-0.1.dev10+ge21
 # Setting up environment and container
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 export ALRB_localConfigDir=$HOME/localConfig
-source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -m /data/ -r "pip3 install  light_roast-0.1.dev10+ge21defc-py3-none-any.whl
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -m /data/ -r "lsetup 'python 3.9.22-x86_64-el9'
+pip3 install  light_roast-0.1.dev10+ge21defc-py3-none-any.whl
 python3.9 example.py  2>&1 | tee coffea_hist.log"
 
 #date >> split.log
