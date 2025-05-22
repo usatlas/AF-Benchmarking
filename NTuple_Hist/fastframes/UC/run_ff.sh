43eadab11639804ed57318e7ef807312cbe81fd6
selbor <jprobles@ucsc.edu>
2025-05-21 17:30:30 -0700

    updating running script for ff

--- NTuple_Hist/fastframes/UC/run_ff.sh
+++ NTuple_Hist/fastframes/UC/run_ff.sh
@@ -16,14 +16,14 @@ fi
 # Sets up ATLAS environment
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 export ALRB_localConfigDir=$HOME/localConfig
-source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -m /data/ -r "asetup StatAnalysis,0.5.0
-  lsetup emi
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -m /data/ -r "asetup StatAnalysis,0.5.0 &&\
+  lsetup emi &&\
 
-  cat $HOME/pass.txt | voms-proxy-init -voms atlas
-  cd /data/$(whoami)/ntuple_hist/TutorialClass/build
-  source setup.sh
-  cd - 
-  date >> split.log
+  cat $HOME/pass.txt | voms-proxy-init -voms atlas &&\
+  cd /data/$(whoami)/ntuple_hist/TutorialClass/buil &&\
+  source setup.sh &&\
+  cd - &&\
+  date >> split.log &&\
   python3 /data/$(whoami)/ntuple_hist/FastFrames/python/FastFrames.py -c /data/$(whoami)/input/example_config.yml 2>&1 | tee ff.log"
 
 # Getting the date and time after running script
