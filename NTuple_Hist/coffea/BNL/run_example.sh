163f41179e516cd2d84b6adc2ae8c1f9adb8a857
selbor <jprobles@ucsc.edu>
2025-05-22 17:56:27 -0700

    fixed a typo

--- NTuple_Hist/coffea/BNL/run_example.sh
+++ NTuple_Hist/coffea/BNL/run_example.sh
@@ -14,11 +14,14 @@ else
 fi
 
 cp ~/AF-Benchmarking/NTuple_Hist/coffea/BNL/example.py .
+cp ~/AF-Benchmarking/NTuple_Hist/coffea/light_roast-0.1.dev10+ge21defc-py3-none-any.whl .
 
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 export ALRB_localConfigDir=$HOME/localConfig
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -m /atlasgpfs01/usatlas/data/ -r "date >> split.log &&\
-python3 example.py 2>&1 | tee coffea_hist.log"
+  pip3 install  light_roast-0.1.dev10+ge21defc-py3-none-any.whl &&\
+  pip3 install atlas_schema &&\
+  python3 example.py 2>&1 | tee coffea_hist.log"
 
 #date >> split.log
 
