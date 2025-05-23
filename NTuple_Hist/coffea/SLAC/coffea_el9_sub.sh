69fb584e4ab554dd2ac1c20553d6c866a27faf49
selbor <jprobles@ucsc.edu>
2025-05-23 15:29:16 -0700

    Updated the submission script used at BNL for the coffea ntuple to hit job

---
+++ NTuple_Hist/coffea/SLAC/coffea_el9_sub.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+#SBATCH --account=atlas:usatlas
+#SBATCH --partition=ampere
+#SBATCH --gpus a100:0
+#SBATCH --job-name=coffea_el9
+#SBATCH --output=output-%j.txt
+#SBATCH --error=output-%j.txt
+#SBATCH --ntasks=1 
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=3g
+#SBATCH --time=0-01:00:00
+
+
+unset KRB5CCNAME
+
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase 
+
+# Use ALRB_CONT_CMDOPTS to provide bind mount, etc. options 
+
+export ALRB_CONT_CMDOPTS="-B /sdf"
+
+export ALRB_CONT_RUNPAYLOAD="source /sdf/home/s/$USER/AF-Benchmarking/EVNT/SLAC/run_evnt_centos_container.sh"
+
+source $ATLAS_LOCAL_ROOT_BASE/user/atlasLocalSetup.sh -c el9 -m /sdf/data/atlas/
+
+
