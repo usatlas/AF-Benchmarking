59026ad7c369b1894041cabba87ce294b8fbcd00
selbor <jprobles@ucsc.edu>
2025-05-23 15:40:37 -0700

    Updated the submission script used at SLAC

--- NTuple_Hist/coffea/SLAC/coffea_el9_sub.sh
+++ NTuple_Hist/coffea/SLAC/coffea_el9_sub.sh
@@ -20,7 +20,7 @@ export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 
 export ALRB_CONT_CMDOPTS="-B /sdf"
 
-export ALRB_CONT_RUNPAYLOAD="source /sdf/home/s/$USER/AF-Benchmarking/EVNT/SLAC/run_evnt_centos_container.sh"
+export ALRB_CONT_RUNPAYLOAD="source /sdf/home/s/$USER/AF-Benchmarking/NTuple_Hist/coffea/SLAC/run_example.sh"
 
 source $ATLAS_LOCAL_ROOT_BASE/user/atlasLocalSetup.sh -c el9 -m /sdf/data/atlas/
 
