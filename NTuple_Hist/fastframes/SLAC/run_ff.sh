f7d093129c28b68064ab25639cf10c1b96de625d
selbor <jprobles@ucsc.edu>
2025-05-23 18:08:47 -0700

    updated the ff job script

--- NTuple_Hist/fastframes/SLAC/run_ff.sh
+++ NTuple_Hist/fastframes/SLAC/run_ff.sh
@@ -8,6 +8,6 @@ cd /sdf/data/atlas/u/selbor/TutorialClass/build
 
 source setup.sh
 
-cd /sdf/scratch/atlas/selbor/Ntuple_Hist/fastframes/
-
 python3 /sdf/data/atlas/u/selbor/FastFrames/python/FastFrames.py -c /sdf/data/atlas/u/selbor/input_ff/example_config.yml 2>&1 | tee ff.log
+
+mv ff.log $HOME
