88790c73f4d1e0efb29173ed1a7afb109379478c
selbor <jprobles@ucsc.edu>
2025-05-23 18:33:00 -0700

    updated script

--- NTuple_Hist/fastframes/SLAC/cron_ff.sh
+++ NTuple_Hist/fastframes/SLAC/cron_ff.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-ssh iana"
-cd /sdf/data/atlas/u/selbor
+ssh iana "
+cd /sdf/data/atlas/u/selbor/
 
-sbatch /sdf/home/s/selbor/AF-Benchmarking/NTuple_Hist/fastframes/SLAC/ff_el9_sub.sh"
+sbatch /sdf/home/s/selbor/AF-Benchmarking/NTuple_Hist/fastframes/SLAC/ff_el9_sub.sh
+"
 
