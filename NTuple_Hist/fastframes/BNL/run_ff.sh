aac384105994d999296041207e92d078d7bc4dd6
selbor <jprobles@ucsc.edu>
2025-06-02 16:35:44 -0700

    Updated the path used to setup the custom class

--- NTuple_Hist/fastframes/BNL/run_ff.sh
+++ NTuple_Hist/fastframes/BNL/run_ff.sh
@@ -7,7 +7,7 @@ curr_date=$(date +"%Y.%m.%dT%H")
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 export ALRB_localConfigDir=$HOME/localConfig
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -r "asetup StatAnalysis,0.5.0 &&\
-  cmake3 -S /srv/TutorialClass/TutorialClass -B /srv/TutorialClass/build &&\
+  cmake3 -S /srv/TutorialClass/ -B /srv/TutorialClass/build &&\
   cmake3 --build /srv/TutorialClass/build -j 16 &&\
   source /srv/TutorialClass/buildsetup.sh &&\
   echo date >> ff.log &&\
