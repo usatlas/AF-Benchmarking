9530726d278a4844d6cdfbf0fd051582d68af670
selbor <jprobles@ucsc.edu>
2025-06-02 18:53:40 -0700

    updated the path to the setup.sh script used by ff

--- NTuple_Hist/fastframes/NERSC/run_ff.sh
+++ NTuple_Hist/fastframes/NERSC/run_ff.sh
@@ -8,9 +8,9 @@ cd /global/cfs/cdirs/m2616/selbor/
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 export ALRB_localConfigDir=$HOME/localConfig
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -r "asetup StatAnalysis,0.5.0 &&\
-  cmake3 -S /srv/TutorialClass/FastFrames -B /srv/TutorialClass/build &&\
+  cmake3 -S /srv/TutorialClass/ -B /srv/TutorialClass/build &&\
   cmake3 --build /srv/TutorialClass/build -j 16 &&\
-  source /srv/TutorialClass/buildsetup.sh &&\
+  source /srv/TutorialClass/build/setup.sh &&\
   echo date >> ff.log &&\
   python3 /srv/FastFrames/python/FastFrames.py -c /srv/input/example_config.yml 2>&1 | tee ff.log"
 echo date >> ff.log
