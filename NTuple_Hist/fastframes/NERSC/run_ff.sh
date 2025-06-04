de7e26b270375c957ff3ada630ebe1b351653c23
Juan <selbor903@gmail.com>
2025-06-04 10:18:48 -0700

    updated the script used at NERSC to run the FastFrames job

--- NTuple_Hist/fastframes/NERSC/run_ff.sh
+++ NTuple_Hist/fastframes/NERSC/run_ff.sh
@@ -8,9 +8,13 @@ cd /global/cfs/cdirs/m2616/selbor/
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 export ALRB_localConfigDir=$HOME/localConfig
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -r "asetup StatAnalysis,0.5.0 &&\
-  cmake3 -S /srv/TutorialClass/FastFrames -B /srv/TutorialClass/build &&\
-  cmake3 --build /srv/TutorialClass/build -j 16 &&\
-  source /srv/TutorialClass/buildsetup.sh &&\
+  cmake3 -S /srv/FastFrames -B /srv/FastFrames/build -DCMAKE_INSTALL_PREFIX=/srv/FastFrames/install &&\
+  cmake3 --build /srv/FastFrames/build -j 16 --target install &&\
+  source /srv/FastFrames/build/setup.sh &&\
+  cmake3 -S /srv/TutorialClass -B /srv/TutorialClass/build -DCMAKE_PREFIX_PATH=/srv/FastFrames/install -DCMAKE_INSTALL_PREFIX=/srv/TutorialClass/install &&\
+  cmake3 --build /srv/TutorialClass/build -j 16 --target install &&\
+  source /srv/TutorialClass/build/setup.sh &&\
+
   echo date >> ff.log &&\
   python3 /srv/FastFrames/python/FastFrames.py -c /srv/input/example_config.yml 2>&1 | tee ff.log"
 echo date >> ff.log
