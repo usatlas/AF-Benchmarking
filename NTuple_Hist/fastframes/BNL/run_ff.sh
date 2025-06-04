24ce20796e07e6c99c3a0f00e64ef3e84c503263
Juan <selbor903@gmail.com>
2025-06-04 15:50:51 -0700

    Testing; updated the coded block dealing with cmake

--- NTuple_Hist/fastframes/BNL/run_ff.sh
+++ NTuple_Hist/fastframes/BNL/run_ff.sh
@@ -6,12 +6,19 @@ curr_date=$(date +"%Y.%m.%dT%H")
 # Sets up ATLAS environment
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 export ALRB_localConfigDir=$HOME/localConfig
-source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -r "asetup StatAnalysis,0.5.0 >> command_output.txt &&\
-  cmake3 -S /srv/TutorialClass/ -B /srv/TutorialClass/build >> command_output.txt &&\
-  cmake3 --build /srv/TutorialClass/build -j 16 >> command_output.txt &&\
-  source /srv/TutorialClass/build/setup.sh >> command_output.txt&&\
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -r "asetup StatAnalysis,0.5.0 &&\
+  cmake3 -S /srv/FastFrames -B /srv/FastFrames/build -DCMAKE_INSTALL_PREFIX=/srv/FastFrames/install &&\
+  cmake3 --build /srv/FastFrames/build -j 16 --target install &&\
+  source /srv/FastFrames/build/setup.sh &&\
+  cmake3 -S /srv/TutorialClass -B /srv/TutorialClass/build -DCMAKE_PREFIX_PATH=/srv/FastFrames/install -DCMAKE_INSTALL_PREFIX=/srv/TutorialClass/install &&\
+  cmake3 --build /srv/TutorialClass/build -j 16 --target install &&\
+  source /srv/TutorialClass/build/setup.sh &&\
+
+  cmake3 -S /srv/TutorialClass/ -B /srv/TutorialClass/build &&\
+  cmake3 --build /srv/TutorialClass/build -j 16 &&\
+  source /srv/TutorialClass/build/setup.sh &&\
   echo date >> ff.log &&\
-  python3 /srv/FastFrames/python/FastFrames.py -c /srv/input/example_config.yml 2>&1 | tee ff.log"
+  python3 /srv/FastFrames/python/FastFrames.py -c /srv/input/example_config.yml 2>&1 | tee ff.log""
 date >> ff.log
 hostname >> ff.log
 
@@ -24,4 +31,3 @@ hostname >> ff.log
 # Moves log to outputdir
 #mv ff.log ${output_dir}
 mv ff.log $HOME
-mv command_output.txt $HOME
