cdbd0ef1560b2e527d8498c005e58f23f0c2eb15
Juan <selbor903@gmail.com>
2025-06-10 11:49:56 -0700

    updated paths used in script

--- NTuple_Hist/fastframes/SLAC/run_ff.sh
+++ NTuple_Hist/fastframes/SLAC/run_ff.sh
@@ -2,24 +2,15 @@
 
 curr_date=$(date +"%Y.%m.%dT%H")
 
-echo "Start of Script"
-
-echo "Current dir"
-
-pwd 
-
-asetup StatAnalysis,0.5.0 >> command_out.txt
+asetup StatAnalysis,0.5.0
 
 cd /sdf/data/atlas/u/selbor/TutorialClass/build
 
-pwd >> command_out.txt
+source setup.sh
 
-source setup.sh >> command_out.txt
+cd -
 
-cd - >> command_out.txt
+python3 /srv/FastFramesTutorial/FastFrames/python/FastFrames.py -c /sdf/data/atlas/u/selbor/input_ff/example_config.yml 2>&1 | tee ff.log
 
-python3 /sdf/data/atlas/u/selbor/FastFrames/python/FastFrames.py -c /sdf/data/atlas/u/selbor/input_ff/example_config.yml 2>&1 | tee ff.log
+mv ff.log /srv/benchmarks/${curr_date}/FF_Hist/
 
-mv ff.log $HOME
-
-mv command_out.txt $HOME
