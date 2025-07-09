07110f2377515ef6174c9e2f3fe935fac97377c9
Juan <selbor903@gmail.com>
2025-07-09 15:43:08 -0700

    update

--- NTuple_Hist/coffea/UC/run_example.sh
+++ NTuple_Hist/coffea/UC/run_example.sh
@@ -17,13 +17,13 @@ fi
 date >> split.log
 
 cp /home/$(whoami)/AF-Benchmarking/NTuple_Hist/coffea/UC/example.py .
-cp /home/$(whoami)/AF-Benchmarking/NTuple_Hist/coffea/light_roast-0.1.dev10+ge21defc-py3-none-any.whl .
+#cp /home/$(whoami)/AF-Benchmarking/NTuple_Hist/coffea/light_roast-0.1.dev10+ge21defc-py3-none-any.whl .
 
 # Setting up environment and container
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 export ALRB_localConfigDir=$HOME/localConfig
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -m /data/ -r "lsetup 'python 3.9.22-x86_64-el9' &&\
-pip3 install  light_roast-0.1.dev10+ge21defc-py3-none-any.whl &&\
+#pip3 install  light_roast-0.1.dev10+ge21defc-py3-none-any.whl &&\
 pip3 install atlas_schema &&\
 python3.9 example.py  2>&1 | tee coffea_hist.log"
 
