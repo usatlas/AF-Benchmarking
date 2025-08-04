cd296024dbe6f200826b2913e0ad8f37b0dfc041
Juan <selbor903@gmail.com>
2025-08-04 13:06:00 -0700

    updated script

--- NTuple_Hist/fastframes/NERSC/run_ff.sh
+++ NTuple_Hist/fastframes/NERSC/run_ff.sh
@@ -7,7 +7,7 @@ cd /global/cfs/cdirs/m2616/selbor/
 # Sets up ATLAS environment
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 export ALRB_localConfigDir=$HOME/localConfig
-source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -r "asetup StatAnalysis,0.5.0 &&\
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -r "asetup StatAnalysis,0.5.3 &&\
   cd /srv/FastFramesTutorial/TutorialClass/build/ &&\
   source setup.sh &&\
   cd - &&\
