1738d988a4b70dc5c42e08195d81bfdb0d588c8f
Juan <selbor903@gmail.com>
2025-08-05 19:32:01 -0700

    updated script to match version update

--- NTuple_Hist/fastframes/BNL/run_ff.sh
+++ NTuple_Hist/fastframes/BNL/run_ff.sh
@@ -7,28 +7,17 @@ cd /atlasgpfs01/usatlas/data/jroblesgo/
 # Sets up ATLAS environment
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 export ALRB_localConfigDir=$HOME/localConfig
-source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -r "asetup StatAnalysis,0.5.3 &&\
-  cmake3 -S /srv/FastFrames -B /srv/FastFrames/build -DCMAKE_INSTALL_PREFIX=/srv/FastFrames/install &&\
-  cmake3 --build /srv/FastFrames/build -j 16 --target install &&\
-  source /srv/FastFrames/build/setup.sh &&\
-  cmake3 -S /srv/TutorialClass -B /srv/TutorialClass/build -DCMAKE_PREFIX_PATH=/srv/FastFrames/install -DCMAKE_INSTALL_PREFIX=/srv/TutorialClass/install &&\
-  cmake3 --build /srv/TutorialClass/build -j 16 --target install &&\
-  source /srv/TutorialClass/build/setup.sh &&\
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -m /atlasgpfs01/usatlas/data/ -c el9 -r "asetup StatAnalysis,0.6.2 &&\
+  source /srv/FastFramesTutorial/TutorialClass/build/setup.sh &&\
+  python3 /srv/FastFramesTutorial/FastFrames/python/FastFrames.py -c /srv/input/mc20e_example_config.yml 2>&1 | tee ff.log"
 
-  cmake3 -S /srv/TutorialClass/ -B /srv/TutorialClass/build &&\
-  cmake3 --build /srv/TutorialClass/build -j 16 &&\
-  source /srv/TutorialClass/build/setup.sh &&\
-  echo date >> ff.log &&\
-  python3 /srv/FastFrames/python/FastFrames.py -c /srv/input/mc20e_example_config.yml 2>&1 | tee ff.log"
-date >> ff.log
 hostname >> ff.log
 
 # output directory
-#output_dir="/atlasgpfs01/usatlas/data/jroblesgo/benchmarks/$curr_date/FF_NTuple"
+output_dir="/atlasgpfs01/usatlas/data/jroblesgo/benchmarks/$curr_date/FF_NTuple"
 
 # Creates output dir
-#mkdir -p ${output_dir}
+mkdir -p ${output_dir}
 
 # Moves log to outputdir
-#mv ff.log ${output_dir}
-mv ff.log $HOME
+mv ff.log ${output_dir}
