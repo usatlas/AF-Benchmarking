83920b229c731b261da6bca8610d72516fa1650e
Juan <selbor903@gmail.com>
2025-08-05 19:52:57 -0700

    updated script to match version update

--- NTuple_Hist/fastframes/NERSC/run_ff.sh
+++ NTuple_Hist/fastframes/NERSC/run_ff.sh
@@ -7,10 +7,8 @@ cd /global/cfs/cdirs/m2616/selbor/
 # Sets up ATLAS environment
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 export ALRB_localConfigDir=$HOME/localConfig
-source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -c el9 -r "asetup StatAnalysis,0.5.3 &&\
-  cd /srv/FastFramesTutorial/TutorialClass/build/ &&\
-  source setup.sh &&\
-  cd - &&\
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh -m /global/cfs/cdirs/m2616/selbor/ -c el9 -r "asetup StatAnalysis,0.6.2 &&\
+  source /srv/FastFramesTutorial/TutorialClass/build/setup.sh &&\
   python3 /srv/FastFramesTutorial/FastFrames/python/FastFrames.py -c /srv/ff_input/mc20e_example_config.yml 2>&1 | tee ff.log"
 hostname >> ff.log
 
