73af279089317d53a041b7126c2aeff8bab3a5dc
Juan <selbor903@gmail.com>
2025-09-02 10:57:15 -0700

    updated script

--- NTuple_Hist/event_loop/UC/run_eventloop_arrays.sh
+++ NTuple_Hist/event_loop/UC/run_eventloop_arrays.sh
@@ -10,16 +10,16 @@ source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
 lsetup "views LCG_107a_ATLAS_2 x86_64-el9-gcc13-opt"
 
 # Getting start date
-date >> eventloop_arrays.log
+date >> split.log
 
 # Running the script
 python3 ~/AF-Benchmarking/NTuple_Hist/event_loop/UC/event_loop_arrays.py 2>&1 | tee eventloop_arrays.log 
 
 # Getting end date
-date >> eventloop_arrays.log
+date >> split.log
 
 # Getting host name
-hostname >> eventloop_arrays.log
+hostname >> split.log
 
 # Output Dir
 output_dir="/data/${whoami}/benchmarks/$curr_time/eventloop_arrays/"
@@ -27,3 +27,4 @@ output_dir="/data/${whoami}/benchmarks/$curr_time/eventloop_arrays/"
 mkdir -p ${output_dir}
 
 mv eventloop_arrays.log ${output_dir}
+mv split.log ${output_dir}
--- NTuple_Hist/event_loop/UC/run_eventloop_noarrays.sh
+++ NTuple_Hist/event_loop/UC/run_eventloop_noarrays.sh
@@ -11,17 +11,17 @@ lsetup "views LCG_107a_ATLAS_2 x86_64-el9-gcc13-opt"
 
 
 # Getting start date
-date >> eventloop_noarrays.log
+date >> split.log
 
 
 # Running the script
 python3 ~/AF-Benchmarking/NTuple_Hist/event_loop/UC/event_loop_noarrays.py 2>&1 | tee eventloop_noarrays.log 
 
 # Getting end date
-date >> eventloop_noarrays.log
+date >> split.log
 
 # Getting host name
-hostname >> eventloop_noarrays.log
+hostname >> split.log
 
 
 # Output Dir
@@ -30,3 +30,4 @@ output_dir="/data/$USER/benchmarks/$curr_time/eventloop_noarrays/"
 mkdir -p ${output_dir}
 
 mv eventloop_noarrays.log ${output_dir}
+mv split.log ${output_dir}
