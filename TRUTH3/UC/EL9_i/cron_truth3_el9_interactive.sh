cb4e5558bcfbc66e2afe06953cf6de33548ff190
Juan <selbor903@gmail.com>
2025-10-24 12:51:36 -0700

    Updated the interactive script so that it cleans the job directory after every run; contains a safety net in case of failure

---
+++ TRUTH3/UC/EL9_i/cron_truth3_el9_interactive.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /home/selbor/TRUTH3Job/el9_i/ || exit
+
+/home/selbor/AF-Benchmarking/TRUTH3/UC/run_truth3_el9_interactive.sh
---
+++ TRUTH3/UC/EL9_i/run_truth3_el9_interactive.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# shellcheck disable=SC1091
+
+curr_time=$(date +"%Y.%m.%dT%H")
+# Current time used for file storage
+
+
+# Defines the directory where the input files are stored
+config_dir="/data/$USER/TRUTH3_StaticDir/"
+
+# Defines the OS the container will have
+OScontainer="el9"
+
+# Sets up the working environment
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+
+# Appends time before Derivation_tf.py to log file
+date +'%H:%H:%S' >> split.log
+
+# Sets up the container:
+## -c : used to make a container followed by the OS we want to use
+## -m : mounts a specific directory
+## -r : precedes the commands we want to run within the container
+# shellcheck disable=SC1091
+source "${ATLAS_LOCAL_ROOT_BASE}"/user/atlasLocalSetup.sh -c "${OScontainer}" -m /data:/data -r "asetup Athena,24.0.53,here && \
+  Derivation_tf.py --CA True --inputEVNTFile ${config_dir}EVNT_el9_interactive.root --outputDAODFile=TRUTH3.root --formats TRUTH3 2>&1 | tee pipe_file.log"
+
+# Appends time after Derivation_tf.py to a log file
+date +'%H:%H:%S' >> split.log
+
+# Defines the output directory
+output_dir="/home/$USER/benchmarks/${curr_time}/TRUTH3_el9_container_interactive"
+
+# Creates the output directory
+mkdir -p "${output_dir}"
+
+# Obtains and appends the host name and payload size to the log file
+hostname >> split.log
+du DAOD_TRUTH3.TRUTH3.root >> split.log
+
+# Moves the log file to the output directory
+mv log.Derivation "${output_dir}"
+mv split.log "${output_dir}"
+mv pipe_file.log "${output_dir}"
+
+# Directory that needs to be cleaned
+cleanup_dir="/home/selbor/TRUTH3Job/el9_i"
+
+if [[ -d "${cleanup_dir}" && "${cleanup_dir}" == "/home/selbor/TRUTH3Job/el9_i" ]]; then
+    rm -rf "${cleanup_dir:?}/"*
+fi
