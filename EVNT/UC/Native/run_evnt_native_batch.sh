ff0d8d47ffc0590e3fe105b265825b4bbac18d02
Juan <selbor903@gmail.com>
2025-11-14 01:14:50 -0800

    updated script

--- EVNT/UC/Native/run_evnt_native_batch.sh
+++ EVNT/UC/Native/run_evnt_native_batch.sh
@@ -5,9 +5,19 @@ curr_time=$(date +"%Y.%m.%dT%H")
 # The seed used in the job
 seed=1001
 
-# Directory storing the input files
-config_dir="EVNT/EVNTFiles/100xxx/100001"
+# shellcheck disable=SC1091
+
+set -euo pipefail
+
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+repo_root="$(cd "${script_dir}/../.." && pwd)"
+
+jo_dir="${repo_root}/EVNT/EVNTFiles/100xxx/100001"
 jo_name="MC100001_MGPy8EG_A14N23LO_MET_25_N2_100_N1_80_WB.py"
+jo_path="${jo_dir}/${jo_name}"
+
+echo "Using JO: ${jo_path}"
+
 
 # Sets up our working environment
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
@@ -19,7 +29,7 @@ date +'%H:%H:%S' >> split.log
 
 # Sets up the Ath* version
 asetup AthGeneration,23.6.34,here
-Gen_tf.py --ecmEnergy=13000.0 --jobConfig="${config_dir}/${jo_name}"  --outputEVNTFile=EVNT.root --maxEvents=10000 --randomSeed="${seed}" 2>&1 | tee pipe_file.log
+Gen_tf.py --ecmEnergy=13000.0 --jobConfig="${jo_path}"  --outputEVNTFile=EVNT.root --maxEvents=10000 --randomSeed="${seed}" 2>&1 | tee pipe_file.log
 
 # Appends time before Gen_tf.py to a log file
 date +'%H:%H:%S' >> split.log
