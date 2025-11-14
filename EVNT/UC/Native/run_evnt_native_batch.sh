379ad1ed26e716f486d3ae518fddd248e4e89c44
Juan <selbor903@gmail.com>
2025-11-14 01:59:31 -0800

    Adding check points

--- EVNT/UC/Native/run_evnt_native_batch.sh
+++ EVNT/UC/Native/run_evnt_native_batch.sh
@@ -8,7 +8,7 @@ seed=1001
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 repo_root="$(cd "${script_dir}/../.." && pwd)"
 
-jo="${repo_root}/EVNTFiles/100xxx/100001/"
+jo="${repo_root}/EVNTFiles/100xxx/100001/MC100001_MGPy8EG_A14N23LO_MET_25_N2_100_N1_80_WB.py"
 
 ls "${jo}"
 
