86ff2b8a0c28e966c2a9b1a06999e167bcfae23e
Juan <selbor903@gmail.com>
2025-11-14 14:00:55 -0800

    updated el9 script used by the evnt job

--- EVNT/UC/EL9/run_evnt_el9_batch.sh
+++ EVNT/UC/EL9/run_evnt_el9_batch.sh
@@ -10,7 +10,7 @@ OS_container="el9"
 seed=1001
 
 # Directory storing the input files
-config_dir="/data/$(whoami)/evnt_el9/100xxx/100001"
+config_dir="${GITHUB_WORKSPACE}/EVNT/EVNTFiles/100xxx/100001"
 
 # Setting up the working environment
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
@@ -23,14 +23,14 @@ date +'%H:%H:%S' >> split.log
 ## -m : mounts a specific directory
 ## -r : precedes the commands we want to run within the container
 # shellcheck disable=SC1091
-source "${ATLAS_LOCAL_ROOT_BASE}"/user/atlasLocalSetup.sh -c ${OS_container} -m /data:/data -r "asetup AthGeneration,23.6.34,here && \
+source "${ATLAS_LOCAL_ROOT_BASE}"/user/atlasLocalSetup.sh -c "${OS_container}" -m /data:/data -r "asetup AthGeneration,23.6.34,here && \
 Gen_tf.py --ecmEnergy=13000.0 --jobConfig=${config_dir}  --outputEVNTFile=EVNT.root --maxEvents=10000 --randomSeed=${seed} 2>&1 | tee pipe_file.log"
 
 # Appends time after Gen_tf.py to a log file
 date +'%H:%H:%S' >> split.log
 
 # Defines and makes the output directory
-output_dir="/home/$(whoami)/benchmarks/${curr_time}/EVNT_contained_el9/"
+output_dir="$HOME/benchmarks/${curr_time}/EVNT_contained_el9/"
 mkdir -p "${output_dir}"
 
 # Obtains and appends the host name and payload size to the log file
@@ -38,7 +38,7 @@ hostname >> split.log
 du EVNT.root >> split.log
 
 # Moves the log file and date_name file to the output directory
-mv log.generate "${output_dir}"
+#mv log.generate "${output_dir}"
 mv split.log "${output_dir}"
 mv pipe_file.log "${output_dir}"
 
