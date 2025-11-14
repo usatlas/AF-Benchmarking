8b058d010fe4efb94c70269d6633289989a93d01
Juan Robles <selbor@login03.af.uchicago.edu>
2025-11-14 02:29:55 -0600

    Vendor SUSY MadGraphControl helpers and GeneratorFilters JOs

---
+++ EVNT/PMG/GeneratorFilters/xAODMETFilter_Common.py
@@ -0,0 +1,16 @@
+# common fragment for xAODMissingEt filter
+# conversion to XAOD, 
+# creation of slimmed container containing MET
+# connecting the filter
+
+include ("GeneratorFilters/CreatexAODSlimContainers.py")
+createxAODSlimmedContainer("TruthMET",prefiltSeq)
+prefiltSeq.xAODCnv.AODContainerName = 'GEN_EVENT'
+
+from GeneratorFilters.GeneratorFiltersConf import xAODMETFilter
+xAODMissingEtFilter = xAODMETFilter("xAODMissingEtFilter")  
+filtSeq += xAODMissingEtFilter
+
+# to modiify cuts put into JOs e.g.:
+#filtSeq.xAODMissingEtFilter.METCut = 12000.0
+#filtSeq.xAODMissingEtFilter.UseNeutrinosFromHadrons = False
