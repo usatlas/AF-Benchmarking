5b861b30e9ec7bb1b4d873b68ea0f9a688e9a784
Juan <selbor903@gmail.com>
2025-11-14 12:25:30 -0800

    testing max events

--- EVNT/UC/Native/run_evnt_native_batch.sh
+++ EVNT/UC/Native/run_evnt_native_batch.sh
@@ -8,7 +8,8 @@ seed=1001
 # Directory storing the input files
 config_dir="${GITHUB_WORKSPACE}/EVNT/EVNTFiles/100xxx/100001"
 
-ls "${config_dir}"
+#max_events=10000
+max_events=100
 
 # Sets up our working environment
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
@@ -20,7 +21,7 @@ date +'%H:%H:%S' >> split.log
 
 # Sets up the Ath* version
 asetup AthGeneration,23.6.34,here
-Gen_tf.py --ecmEnergy=13000.0 --jobConfig="${config_dir}"  --outputEVNTFile=EVNT.root --maxEvents=10000 --randomSeed="${seed}" 2>&1 | tee pipe_file.log
+Gen_tf.py --ecmEnergy=13000.0 --jobConfig="${config_dir}"  --outputEVNTFile=EVNT.root --maxEvents="${max_events}" --randomSeed="${seed}" 2>&1 | tee pipe_file.log
 
 # Appends time before Gen_tf.py to a log file
 date +'%H:%H:%S' >> split.log
