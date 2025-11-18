96214f1a873f8ca7aa21699892437c04a1699632
Juan <selbor903@gmail.com>
2025-11-18 14:30:07 -0800

    updated the script

--- NTuple_Hist/coffea/UC/run_example.sh
+++ NTuple_Hist/coffea/UC/run_example.sh
@@ -14,6 +14,8 @@ export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 export ALRB_localConfigDir="$HOME"/localConfig
 # shellcheck disable=SC1091
 source "${ATLAS_LOCAL_ROOT_BASE}"/user/atlasLocalSetup.sh -c el9 -r "lsetup 'python 3.9.22-x86_64-el9' &&\
+  pip3 install ${GITHUB_WORKSPACE}/NTuple_Hist/coffea/light_roast-0.1.dev10+ge21defc-py3-none-any.whl &&\
+  pip3 install atlas_schema &&\
 python3.9 ${GITHUB_WORKSPACE}/NTuple_Hist/coffea/UC/example.py  2>&1 | tee coffea_hist.log"
 
 date >> split.log
