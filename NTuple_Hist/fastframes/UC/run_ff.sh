15506c16d6ab58958dc3724cfea5e71f26b6e38c
selbor <selbor903@gmail.com>
2025-11-19 19:05:55 -0800

    updated paths

--- NTuple_Hist/fastframes/UC/run_ff.sh
+++ NTuple_Hist/fastframes/UC/run_ff.sh
@@ -2,6 +2,8 @@
 
 curr_date=$(date +"%Y.%m.%dT%H")
 
+yml_dir="${GITHUB_WORKSPACE}/NTuple_Hist/fastframes/UC/"
+
 # Sets up our working environment
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 # shellcheck disable=SC1091
@@ -16,7 +18,7 @@ source /data/selbor/FastFramesTutorial/TutorialClass/build/setup.sh
 
 date >> split.log
 
-python3 /data/selbor/FastFramesTutorial/FastFrames/python/FastFrames.py -c mc20e_example_config.yml 2>&1 | tee ff.log
+python3 /data/selbor/FastFramesTutorial/FastFrames/python/FastFrames.py -c "${yml_dir}"mc20e_example_config.yml 2>&1 | tee ff.log
 
 # Getting the date and time after running script
 date >> split.log
