833f534cbf6ca7cbaa983e96a637b4ac861785ad
Juan <selbor903@gmail.com>
2025-12-15 22:10:25 -0800

    Removed code block that was creating dirs

--- NTuple_Hist/coffea/UC/run_example.sh
+++ NTuple_Hist/coffea/UC/run_example.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Gets the current time
-
-curr_time=$(date +"%Y.%m.%dT%H")
-
 date >> split.log
 
 #cp ${GITHUB_WORKSPACE}/NTuple_Hist/coffea/UC/example.py .
@@ -18,13 +14,7 @@ source "${ATLAS_LOCAL_ROOT_BASE}"/user/atlasLocalSetup.sh -c el9 -m /data -r "ls
   pip3 install atlas_schema &&\
 python3.9 ${GITHUB_WORKSPACE}/NTuple_Hist/coffea/UC/example.py  2>&1 | tee coffea_hist.log"
 
-date >> split.log
-
-hostname >> split.log
-
-# Directory that needs to be cleaned
-cleanup_dir="/home/selbor/ntuple/coffea"
-
-if [[ -d "${cleanup_dir}" && "${cleanup_dir}" == "/home/selbor/ntuple/coffea" ]]; then
-    rm -rf "${cleanup_dir:?}/"*
-fi
+{
+  date+'%H:%M:%S'
+  hostname
+} >> split.log
