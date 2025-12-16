6b667a24e066968ddedcad1662ea49fdcc6b8c4a
Juan <selbor903@gmail.com>
2025-12-15 21:21:04 -0800

    removed code blocks that create directories move files

--- EVNT/UC/EL9/run_evnt_el9_batch.sh
+++ EVNT/UC/EL9/run_evnt_el9_batch.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Current time used for log file storage
-curr_time=$(date +"%Y.%m.%dT%H")
-
 # Defining the OS wanted in the container
 OS_container="el9"
 
@@ -27,24 +24,8 @@ source "${ATLAS_LOCAL_ROOT_BASE}"/user/atlasLocalSetup.sh -c "${OS_container}" -
 Gen_tf.py --ecmEnergy=13000.0 --jobConfig=${config_dir}  --outputEVNTFile=EVNT.root --maxEvents=10000 --randomSeed=${seed} 2>&1 | tee pipe_file.log"
 
 # Appends time after Gen_tf.py to a log file
-date +'%H:%H:%S' >> split.log
-
-# Defines and makes the output directory
-output_dir="$HOME/benchmarks/${curr_time}/EVNT_contained_el9/"
-mkdir -p "${output_dir}"
-
-# Obtains and appends the host name and payload size to the log file
-hostname >> split.log
-du EVNT.root >> split.log
-
-# Moves the log file and date_name file to the output directory
-#mv log.generate "${output_dir}"
-mv split.log "${output_dir}"
-mv pipe_file.log "${output_dir}"
-
-# Directory that needs to be cleaned
-cleanup_dir="/home/selbor/EVNTJob/el9"
-
-if [[ -d "${cleanup_dir}" && "${cleanup_dir}" == "/home/selbor/EVNTJob/el9" ]]; then
-    rm -rf "${cleanup_dir:?}/"*
-fi
+{
+  date +'%H:%M:%S'
+  hostname
+  du EVNT.root
+} >> split.logi
