7feaa415786e7eb05289a853ae2faa24a5cc1ed5
Juan <selbor903@gmail.com>
2025-12-17 20:30:07 -0800

    update path

--- TRUTH3/BNL/EL9/cron_el_batch.sh
+++ TRUTH3/BNL/EL9/cron_el_batch.sh
@@ -5,5 +5,5 @@ job_dir="/atlasgpfs01/usatlas/scratch/jroblesgo/TRUTH3/el"
 
 if [ -d ${job_dir} ]; then
   cd "${job_dir}" || exit
-  condor_submit ~/AF-Benchmarking/TRUTH3/BNL/truth3_el.sub
+  condor_submit ~/AF-Benchmarking/TRUTH3/BNL/EL9/truth3_el.sub
 fi
