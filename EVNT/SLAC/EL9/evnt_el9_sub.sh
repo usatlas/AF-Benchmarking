8b022c3b3c30554823da37dc2cd035aed31c7e13
Juan <selbor903@gmail.com>
2025-12-18 13:55:05 -0800

    updated paths

--- EVNT/SLAC/EL9/evnt_el9_sub.sh
+++ EVNT/SLAC/EL9/evnt_el9_sub.sh
@@ -3,7 +3,7 @@
 #SBATCH --account=atlas:usatlas
 #SBATCH --partition=ampere
 #SBATCH --gpus a100:0
-#SBATCH --job-name=coffea_batch_el9
+#SBATCH --job-name=evnt_batch_el9
 #SBATCH --output=output-%j.txt
 #SBATCH --error=output-%j.txt
 #SBATCH --ntasks=1
@@ -19,7 +19,7 @@ export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 
 export ALRB_CONT_CMDOPTS="-B /sdf"
 
-export ALRB_CONT_RUNPAYLOAD="source $HOME/AF-Benchmarking/EVNT/SLAC/run_evnt_el_container.sh"
+export ALRB_CONT_RUNPAYLOAD="source $HOME/AF-Benchmarking/EVNT/SLAC/EL9/run_evnt_el_container.sh"
 
 # shellcheck disable=SC1091
 source $ATLAS_LOCAL_ROOT_BASE/user/atlasLocalSetup.sh -c el9 -m /sdf/data/atlas/
