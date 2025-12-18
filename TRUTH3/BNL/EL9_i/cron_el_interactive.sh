f17878524d475cb97b43b46f2c7433558ac660f1
Juan <selbor903@gmail.com>
2025-12-17 20:30:44 -0800

    update path

--- TRUTH3/BNL/EL9_i/cron_el_interactive.sh
+++ TRUTH3/BNL/EL9_i/cron_el_interactive.sh
@@ -5,5 +5,5 @@ job_dir="/atlasgpfs01/usatlas/data/jroblesgo/TRUTH3Job/el_i/"
 
 if [ -d ${job_dir} ]; then
   cd "${job_dir}" || exit
-  /usatlas/u/jroblesgo/AF-Benchmarking/TRUTH3/BNL/run_truth3_el9_interactive.sh
+  /usatlas/u/jroblesgo/AF-Benchmarking/TRUTH3/BNL/EL9_i/run_truth3_el9_interactive.sh
 fi
