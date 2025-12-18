8dbfe63942ab9838ceff5144f3e2606cddef8b8c
Juan <selbor903@gmail.com>
2025-12-18 11:37:39 -0800

    updated path in job

--- TRUTH3/BNL/Native/cron_native_batch.sh
+++ TRUTH3/BNL/Native/cron_native_batch.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 # Job directory
-job_dir="/atlasgpfs01/usatlas/scratch/jroblesgo/TRUTH3/native"
+job_dir="$HOME/TRUTH3Job/native"
 
-if [ -d ${job_dir} ]; then
+if [ -d "${job_dir}" ]; then
   cd "${job_dir}" || exit
   condor_submit ~/AF-Benchmarking/TRUTH3/BNL/Native/truth3_native.sub
 fi
